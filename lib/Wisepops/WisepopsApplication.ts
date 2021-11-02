89966c491449fb6ea857d3f0408bd4b4f0ae59aa
Jakub Sůva <suva.j@hanaboso.com>
2021-11-02 14:48:59 +0100

    Wisepops application

--- lib/Wisepops/WisepopsApplication.ts
+++ lib/Wisepops/WisepopsApplication.ts
@@ -11,32 +11,33 @@ import Field from 'pipes-nodejs-sdk/dist/lib/Application/Model/Form/Field';
 import FieldType from 'pipes-nodejs-sdk/dist/lib/Application/Model/Form/FieldType';
 import ResponseDto from 'pipes-nodejs-sdk/dist/lib/Transport/Curl/ResponseDto';
 
+const API_KEY = 'api_key';
+const WISEPOOPS_URL = 'https://app.wisepops.com/api1/hooks';
+const EMAIL_EVENT = 'email';
+
 export default class WisepopsApplication extends ABasicApplication implements IWebhookApplication {
+  public getDescription = (): string => 'Build website popups.';
   
-  private API_KEY = 'api_key';
-  private WISEPOOPS_URL = 'https://app.wisepops.com/api1/hooks';
-  private EMAIL_EVENT = 'email';
+  public getName = (): string => 'wisepops';
   
-  public getDescription(): string {
-    return 'Build website popups.';
-  }
+  public getPublicName = (): string => 'Wisepops';
   
-  public getName(): string {
-    return 'Wisepops';
-  }
-  
-  public getPublicName(): string {
-    return '';
-  }
-  
-  public getRequestDto(dto: ProcessDto, applicationInstall: ApplicationInstall, method: string | HttpMethods, url?: string, data?: string): RequestDto {
+  public getRequestDto = (
+    dto: ProcessDto,
+    applicationInstall: ApplicationInstall,
+    method: string | HttpMethods,
+    url?: string,
+    data?: string,
+  ): RequestDto => {
     const request = new RequestDto(this.getUri(url)
     .toString(), parseHttpMethod(method));
     request.headers = {
       'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'Authorization':
-        `WISEPOPS-API key="${applicationInstall.getSettings()[FORM][this.API_KEY]}"`
+      /* eslint-disable @typescript-eslint/naming-convention */
+      Accept: 'application/json',
+      Authorization:
+      /* eslint-enable @typescript-eslint/naming-convention */
+        `WISEPOPS-API key="${applicationInstall.getSettings()[FORM][API_KEY]}"`,
     };
     
     if (data) {
@@ -44,46 +45,44 @@ export default class WisepopsApplication extends ABasicApplication implements IW
     }
     
     return request;
-    
-  }
+  };
   
-  public getSettingsForm(): Form {
-    return new Form().addField(new Field(FieldType.TEXT, this.API_KEY, 'API Key', undefined, true));
-  }
+  public getSettingsForm = (): Form => new Form()
+  .addField(new Field(FieldType.TEXT, API_KEY, 'API Key', undefined, true));
   
-  public getWebhookSubscribeRequestDto(applicationInstall: ApplicationInstall, subscription: WebhookSubscription, url: string): RequestDto {
+  public getWebhookSubscribeRequestDto = (
+    applicationInstall: ApplicationInstall,
+    subscription: WebhookSubscription,
+    url: string,
+  ): RequestDto => {
     const request = new ProcessDto();
-    return this.getRequestDto(request, applicationInstall, HttpMethods.POST, this.WISEPOOPS_URL,
+    return this.getRequestDto(request, applicationInstall, HttpMethods.POST, WISEPOOPS_URL,
       JSON.stringify({
-        'target_url': url,
-        'event': subscription.getParameters()['name'],
-      })
-    );
-  }
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        target_url: url,
+        event: subscription.getParameters().name,
+      }));
+  };
   
-  public getWebhookSubscriptions(): WebhookSubscription[] {
-    return [
-      new WebhookSubscription('Collected Emails', 'Webhook', '', { 'name': this.EMAIL_EVENT }),
-    ];
-  }
+  public getWebhookSubscriptions = (): WebhookSubscription[] => [
+    new WebhookSubscription('Collected Emails', 'Webhook', '', { name: EMAIL_EVENT }),
+  ];
   
-  public getWebhookUnsubscribeRequestDto(applicationInstall: ApplicationInstall, id: string): RequestDto {
+  public getWebhookUnsubscribeRequestDto = (applicationInstall: ApplicationInstall, id: string): RequestDto => {
     const request = new ProcessDto();
     return this.getRequestDto(
       request,
       applicationInstall,
       HttpMethods.DELETE,
-      `${this.WISEPOOPS_URL}?hook_id=${id}`,
+      `${WISEPOOPS_URL}?hook_id=${id}`,
     );
-  }
+  };
   
-  public processWebhookSubscribeResponse(dto: ResponseDto, applicationInstall: ApplicationInstall): string {
-    applicationInstall;
-    return JSON.parse(dto.body)['id'];
-  }
+  public processWebhookSubscribeResponse = (
+    dto: ResponseDto,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    applicationInstall: ApplicationInstall,
+  ): string => JSON.parse(dto.body).id;
   
-  public processWebhookUnsubscribeResponse(dto: ResponseDto): boolean {
-    return dto.responseCode === 200;
-  }
-  
+  public processWebhookUnsubscribeResponse = (dto: ResponseDto): boolean => dto.responseCode === 200;
 }
