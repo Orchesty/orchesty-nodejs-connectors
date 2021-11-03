a2c1acd1f5ec038931e2a8b6be1f6356aae84373
Jakub Sůva <suva.j@hanaboso.com>
2021-11-03 09:08:54 +0100

    Zendesk application

--- lib/Zendesk/ZendeskApplication.ts
+++ lib/Zendesk/ZendeskApplication.ts
@@ -9,63 +9,53 @@ import Field from 'pipes-nodejs-sdk/dist/lib/Application/Model/Form/Field';
 import { CLIENT_ID, CLIENT_SECRET } from 'pipes-nodejs-sdk/dist/lib/Authorization/Type/OAuth2/IOAuth2Application';
 import FieldType from 'pipes-nodejs-sdk/dist/lib/Application/Model/Form/FieldType';
 
+const AUTH_URL = 'https://%s.zendesk.com/oauth/authorizations/new';
+
+const SUBDOMAIN = 'subdomain';
+
 export default class ZendeskApplication extends AOAuth2Application {
-  
-  protected SCOPE_SEPARATOR = ScopeSeparatorEnum.SPACE;
-  
-  private AUTH_URL = 'https://%s.zendesk.com/oauth/authorizations/new';
-  private TOKEN_URL = 'https://%s.zendesk.com/oauth/tokens';
-  private SUBDOMAIN = 'subdomain';
-  private SCOPES = ['read', 'write'];
-  
-  public getAuthUrl(): string {
-    return this.AUTH_URL;
-  }
-  
-  public getDescription(): string {
-    return 'Zendesk is a customer support software. It helps companies and organisations manage customer queries and problems through a ticketing system.';
-  }
-  
-  public getName(): string {
-    return 'zendesk';
-  }
-  
-  public getPublicName(): string {
-    return 'Zendesk';
-  }
-  
-  public getRequestDto(dto: ProcessDto, applicationInstall: ApplicationInstall, method: string | HttpMethods, url?: string, data?: string): RequestDto | Promise<RequestDto> {
+  protected _getScopesSeparator = (): string => ScopeSeparatorEnum.SPACE;
+
+  public getAuthUrl = (): string => AUTH_URL;
+
+  // eslint-disable-next-line max-len
+  public getDescription = (): string => 'Zendesk is a customer support software. It helps companies and organisations manage customer queries and problems through a ticketing system.';
+
+  public getName = (): string => 'zendesk';
+
+  public getPublicName = (): string => 'Zendesk';
+
+  public getRequestDto = (
+    dto: ProcessDto,
+    applicationInstall: ApplicationInstall,
+    method: string | HttpMethods,
+    url?: string,
+    data?: string,
+  ): RequestDto | Promise<RequestDto> => {
     const request = new RequestDto(this.getUri(url)
-    .toString(), parseHttpMethod(method));
+      .toString(), parseHttpMethod(method));
     request.headers = {
       'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'Authorization': `Bearer ${this.getAccessToken(applicationInstall)}`
+      /* eslint-disable @typescript-eslint/naming-convention */
+      Accept: 'application/json',
+      Authorization: `Bearer ${this.getAccessToken(applicationInstall)}`,
+      /* eslint-enable @typescript-eslint/naming-convention */
     };
-    
+
     if (data) {
       request.body = data;
     }
-    
+
     return request;
-    
-  }
-  
-  public getScopes(applicationInstall: ApplicationInstall): string[] {
-    applicationInstall;
-    return this.SCOPES;
-  }
-  
-  public getSettingsForm(): Form {
-    return (new Form())
-    .addField((new Field(FieldType.TEXT, this.SUBDOMAIN, 'Subdomain', undefined, true)))
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public getScopes = (applicationInstall: ApplicationInstall): string[] => ['read', 'write'];
+
+  public getSettingsForm = (): Form => (new Form())
+    .addField((new Field(FieldType.TEXT, SUBDOMAIN, 'Subdomain', undefined, true)))
     .addField((new Field(FieldType.TEXT, CLIENT_ID, 'Client Id', undefined, true)))
     .addField((new Field(FieldType.TEXT, CLIENT_SECRET, 'Client Secret', undefined, true)));
-    
-  }
-  
-  public getTokenUrl(): string {
-    return this.TOKEN_URL;
-  }
-  
+
+  public getTokenUrl = (): string => 'https://%s.zendesk.com/oauth/tokens';
 }
