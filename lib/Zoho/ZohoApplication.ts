2734698221b4f41a76546e55cae60ffdc12dec29
Jakub Sůva <suva.j@hanaboso.com>
2021-11-03 10:08:37 +0100

    Zoho application

--- lib/Zoho/ZohoApplication.ts
+++ lib/Zoho/ZohoApplication.ts
@@ -9,61 +9,51 @@ import RequestDto from 'pipes-nodejs-sdk/dist/lib/Transport/Curl/RequestDto';
 import ProcessDto from 'pipes-nodejs-sdk/dist/lib/Utils/ProcessDto';
 import HttpMethods, { parseHttpMethod } from 'pipes-nodejs-sdk/dist/lib/Transport/HttpMethods';
 
+const AUTH_URL = 'https://accounts.zoho.eu/oauth/v2/auth';
+
 export default class ZohoApplication extends AOAuth2Application {
-  
-  protected SCOPE_SEPARATOR = ScopeSeparatorEnum.SPACE;
-  
-  private AUTH_URL = 'https://accounts.zoho.eu/oauth/v2/auth';
-  private TOKEN_URL = 'https://accounts.zoho.eu/oauth/v2/token';
-  private SCOPES = ['ZohoCRM.modules.ALL', 'ZohoCRM.settings.ALL'];
-  
-  public getAuthUrl(): string {
-    return this.AUTH_URL;
-  }
-  
-  public getDescription(): string {
-    return 'Zoho is a provider of a Customer Relationship Management (CRM) solution';
-  }
-  
-  public getName(): string {
-    return 'zoho';
-  }
-  
-  public getPublicName(): string {
-    return 'Zoho';
-  }
-  
-  getRequestDto(dto: ProcessDto, applicationInstall: ApplicationInstall, method: string | HttpMethods, url?: string, data?: string): RequestDto | Promise<RequestDto> {
+  public getAuthUrl = (): string => AUTH_URL;
+
+  public getDescription = (): string => 'Zoho is a provider of a Customer Relationship Management (CRM) solution';
+
+  public getName = (): string => 'zoho';
+
+  public getPublicName = (): string => 'Zoho';
+
+  public getRequestDto(
+    dto: ProcessDto,
+    applicationInstall: ApplicationInstall,
+    method: string | HttpMethods,
+    url?: string,
+    data?: string,
+  ): RequestDto | Promise<RequestDto> {
     const request = new RequestDto(this.getUri(url)
-    .toString(), parseHttpMethod(method));
-    request.headers =
-      {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': `Bearer ${this.getAccessToken(applicationInstall)}`,
-      };
-    
+      .toString(), parseHttpMethod(method));
+    request.headers = {
+      'Content-Type': 'application/json',
+      /* eslint-disable @typescript-eslint/naming-convention */
+      Accept: 'application/json',
+      Authorization: `Bearer ${this.getAccessToken(applicationInstall)}`,
+      /* eslint-enable @typescript-eslint/naming-convention */
+    };
+
     if (data) {
       request.body = data;
     }
-    
+
     return request;
-    
   }
-  
-  public getScopes(applicationInstall: ApplicationInstall): string[] {
-    applicationInstall;
-    return this.SCOPES;
-  }
-  
-  public getSettingsForm(): Form {
-    return new Form()
+
+  public getScopes = (
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    applicationInstall: ApplicationInstall,
+  ): string[] => ['ZohoCRM.modules.ALL', 'ZohoCRM.settings.ALL'];
+
+  public getSettingsForm = (): Form => new Form()
     .addField(new Field(FieldType.TEXT, CLIENT_ID, 'Client Id', undefined, true))
     .addField(new Field(FieldType.TEXT, CLIENT_SECRET, 'Client Secret', undefined, true));
-  }
-  
-  public getTokenUrl(): string {
-    return this.TOKEN_URL;
-  }
-  
+
+  public getTokenUrl = (): string => 'https://accounts.zoho.eu/oauth/v2/token';
+
+  protected _getScopesSeparator = (): string => ScopeSeparatorEnum.SPACE;
 }
