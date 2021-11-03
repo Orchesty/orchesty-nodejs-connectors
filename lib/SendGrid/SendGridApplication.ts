717d4da13607739aa7e5d9921e4338106ad8d52f
Jakub Sůva <suva.j@hanaboso.com>
2021-11-03 15:15:27 +0000

    SendGrid application

---
+++ lib/SendGrid/SendGridApplication.ts
@@ -0,0 +1,54 @@
+import { ABasicApplication } from 'pipes-nodejs-sdk/dist/lib/Authorization/Type/Basic/ABasicApplication';
+import RequestDto from 'pipes-nodejs-sdk/dist/lib/Transport/Curl/RequestDto';
+import HttpMethods, { parseHttpMethod } from 'pipes-nodejs-sdk/dist/lib/Transport/HttpMethods';
+import { ApplicationInstall } from 'pipes-nodejs-sdk/dist/lib/Application/Database/ApplicationInstall';
+import ProcessDto from 'pipes-nodejs-sdk/dist/lib/Utils/ProcessDto';
+import Form from 'pipes-nodejs-sdk/dist/lib/Application/Model/Form/Form';
+import FieldType from 'pipes-nodejs-sdk/dist/lib/Application/Model/Form/FieldType';
+import Field from 'pipes-nodejs-sdk/dist/lib/Application/Model/Form/Field';
+import { AUTHORIZATION_SETTINGS } from 'pipes-nodejs-sdk/dist/lib/Application/Base/AApplication';
+import { CommonHeaders } from 'pipes-nodejs-sdk/dist/lib/Utils/Headers';
+
+export const BASE_URL = 'https://api.sendgrid.com/v3';
+
+const API_KEY = 'api_key';
+
+export default class SendGridApplication extends ABasicApplication {
+  public getDescription = (): string => 'Send Email With Confidence.';
+
+  public getName = (): string => 'send-grid';
+
+  public getPublicName = (): string => 'SendGrid';
+
+  public getRequestDto(
+    _dto: ProcessDto,
+    applicationInstall: ApplicationInstall,
+    method: string | HttpMethods,
+    url?: string,
+    data?: string,
+  ): RequestDto | Promise<RequestDto> {
+    if (!this.isAuthorized(applicationInstall)) {
+      throw new Error('Application SendGrid is not authorized!');
+    }
+
+    const settings = applicationInstall.getSettings();
+    const token = settings[AUTHORIZATION_SETTINGS][API_KEY];
+    const dto = new RequestDto(
+      new URL(url ?? BASE_URL).toString(),
+      parseHttpMethod(method),
+      JSON.stringify({
+        [CommonHeaders.CONTENT_TYPE]: 'application/json',
+        [CommonHeaders.AUTHORIZATION]: `Bearer ${token}`,
+      }),
+    );
+    if (data) {
+      dto.body = data;
+    }
+
+    return dto;
+  }
+
+  public getSettingsForm(): Form {
+    return new Form().addField(new Field(FieldType.TEXT, API_KEY, 'Api key', undefined, true));
+  }
+}
