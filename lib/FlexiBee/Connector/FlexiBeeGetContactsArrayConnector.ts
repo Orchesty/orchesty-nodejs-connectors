c96c9a16286f8406cdaeb094f0edd9670032c2f8
Stepan Zdrazil <zdrazil.s@hanaboso.com>
2021-11-03 08:17:04 +0100

    add connector FlexiBee

---
+++ lib/FlexiBee/Connector/FlexiBeeGetContactsArrayConnector.ts
@@ -0,0 +1,26 @@
+import AConnector from 'pipes-nodejs-sdk/dist/lib/Connector/AConnector';
+import ProcessDto from 'pipes-nodejs-sdk/dist/lib/Utils/ProcessDto';
+import HttpMethods from 'pipes-nodejs-sdk/dist/lib/Transport/HttpMethods';
+import FlexiBeeApplication from '../FexiBeeApplication';
+
+export default class FlexiBeeGetContactsArrayConnector extends AConnector {
+  public getName = (): string => 'flexibee-get-contacts-array';
+
+  public async processAction(_dto: ProcessDto): Promise<ProcessDto> {
+    const dto = _dto;
+    const applicationInstall = await this._getApplicationInstallFromHeaders(dto);
+
+    const application = this._application as FlexiBeeApplication;
+    const request = await application.getRequestDto(
+      dto,
+      applicationInstall,
+      HttpMethods.GET,
+      application.getUrl(applicationInstall, 'kontakt.json'),
+    );
+    request.debugInfo = dto;
+    const response = await this._sender.send(request);
+    this.evaluateStatusCode(response, dto);
+    dto.data = response.body;
+    return dto;
+  }
+}
