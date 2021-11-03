b5aeed55d0964e69fe423be4dbcfce1632961d75
Jakub Sůva <suva.j@hanaboso.com>
2021-11-03 12:20:52 +0100

    Zendesk application

--- lib/Zendesk/ZendeskApplication.ts
+++ lib/Zendesk/ZendeskApplication.ts
@@ -8,16 +8,12 @@ import HttpMethods, { parseHttpMethod } from 'pipes-nodejs-sdk/dist/lib/Transpor
 import Field from 'pipes-nodejs-sdk/dist/lib/Application/Model/Form/Field';
 import { CLIENT_ID, CLIENT_SECRET } from 'pipes-nodejs-sdk/dist/lib/Authorization/Type/OAuth2/IOAuth2Application';
 import FieldType from 'pipes-nodejs-sdk/dist/lib/Application/Model/Form/FieldType';
-
-const AUTH_URL = 'https://%s.zendesk.com/oauth/authorizations/new';
+import { FORM } from 'pipes-nodejs-sdk/dist/lib/Application/Base/AApplication';
+import { CommonHeaders } from 'pipes-nodejs-sdk/dist/lib/Utils/Headers';
 
 const SUBDOMAIN = 'subdomain';
 
 export default class ZendeskApplication extends AOAuth2Application {
-  protected _getScopesSeparator = (): string => ScopeSeparatorEnum.SPACE;
-
-  public getAuthUrl = (): string => AUTH_URL;
-
   // eslint-disable-next-line max-len
   public getDescription = (): string => 'Zendesk is a customer support software. It helps companies and organisations manage customer queries and problems through a ticketing system.';
 
@@ -35,11 +31,9 @@ export default class ZendeskApplication extends AOAuth2Application {
     const request = new RequestDto(this.getUri(url)
       .toString(), parseHttpMethod(method));
     request.headers = {
-      'Content-Type': 'application/json',
-      /* eslint-disable @typescript-eslint/naming-convention */
-      Accept: 'application/json',
-      Authorization: `Bearer ${this.getAccessToken(applicationInstall)}`,
-      /* eslint-enable @typescript-eslint/naming-convention */
+      [CommonHeaders.CONTENT_TYPE]: 'application/json',
+      [CommonHeaders.ACCEPT]: 'application/json',
+      [CommonHeaders.AUTHORIZATION]: `Bearer ${this.getAccessToken(applicationInstall)}`,
     };
 
     if (data) {
@@ -57,5 +51,17 @@ export default class ZendeskApplication extends AOAuth2Application {
     .addField((new Field(FieldType.TEXT, CLIENT_ID, 'Client Id', undefined, true)))
     .addField((new Field(FieldType.TEXT, CLIENT_SECRET, 'Client Secret', undefined, true)));
 
-  public getTokenUrl = (): string => 'https://%s.zendesk.com/oauth/tokens';
+  public getAuthUrlWithSubdomain = (applicationInstall: ApplicationInstall): string => `https://${applicationInstall.getSettings()[FORM][SUBDOMAIN]}.zendesk.com/oauth/authorizations/new`;
+
+  public getAuthUrl(): string {
+    throw new Error(`Dont use [${this.getAuthUrl.name}] use [${this.getAuthUrlWithSubdomain.name}] instead.`);
+  }
+
+  public getTokenUrlWithSubdomain = (applicationInstall: ApplicationInstall): string => `https://${applicationInstall.getSettings()[FORM][SUBDOMAIN]}.zendesk.com/oauth/tokens`;
+
+  public getTokenUrl(): string {
+    throw new Error(`Dont use [${this.getAuthUrl.name}] use [${this.getTokenUrlWithSubdomain.name}] instead.`);
+  }
+
+  protected _getScopesSeparator = (): string => ScopeSeparatorEnum.SPACE;
 }
