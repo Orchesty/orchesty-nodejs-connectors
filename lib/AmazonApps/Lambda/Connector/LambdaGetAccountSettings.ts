01f722a43727f805144b5ad54fa395e6dea75142
Jakub <sobotka.j@hanaboso.com>
2022-01-12 11:38:24 +0100

    lambdaGetAccountSettins connector

---
+++ lib/AmazonApps/Lambda/Connector/LambdaGetAccountSettings.ts
@@ -0,0 +1,28 @@
+import ProcessDto from 'pipes-nodejs-sdk/dist/lib/Utils/ProcessDto';
+import { GetAccountSettingsCommand, GetAccountSettingsCommandInput } from '@aws-sdk/client-lambda';
+import OnRepeatException from 'pipes-nodejs-sdk/dist/lib/Exception/OnRepeatException';
+import ALambdaObjectConnector from './ALambdaObjectConnector';
+import LambdaApplication from '../LambdaApplication';
+
+export default class LambdaGetAccountSettings extends ALambdaObjectConnector {
+  protected _getCustomId = (): string => 'get-account-settings';
+
+  processAction = async (_dto: ProcessDto): Promise<ProcessDto> => {
+    const dto = _dto;
+
+    const applicationInstall = await this._getApplicationInstallFromHeaders(dto);
+    const application = this._application as LambdaApplication;
+    const client = application.getLambdaClient(applicationInstall);
+
+    const input: GetAccountSettingsCommandInput = {};
+
+    const command = new GetAccountSettingsCommand(input);
+    try {
+      await client.send(command);
+    } catch (e) {
+      throw new OnRepeatException(60, 10, (e as Error)?.message ?? 'Unknown error.');
+    }
+
+    return dto;
+  };
+}
