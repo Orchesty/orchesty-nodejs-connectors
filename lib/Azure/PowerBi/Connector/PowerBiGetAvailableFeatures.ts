88c0e9d6f3d88c6ecc52a6749bbaaf7040ea1507
Jakub <sobotka.j@hanaboso.com>
2022-01-24 15:08:06 +0100

    wip

--- lib/Azure/PowerBi/Connector/PowerBiGetAvailableFeatures.ts
+++ lib/Azure/PowerBi/Connector/PowerBiGetAvailableFeatures.ts
@@ -1,33 +1,23 @@
 import ProcessDto from 'pipes-nodejs-sdk/dist/lib/Utils/ProcessDto';
-import { GetObjectCommand, GetObjectCommandInput, GetObjectCommandOutput } from '@aws-sdk/client-s3';
 import OnRepeatException from 'pipes-nodejs-sdk/dist/lib/Exception/OnRepeatException';
+import { PipelineRequest } from '@azure/core-rest-pipeline';
 import APowerBiObjectConnector from './APowerBiObjectConnector';
-import { CONTENT, NAME } from '../../../AmazonApps/AAwsObjectConnector';
-import S3Application, { BUCKET } from '../../../AmazonApps/S3/S3Application';
-import { KEY } from '../../../AmazonApps/AAwsApplication';
+import PowerBiApplication from '../PowerBiApplication';
 
 export default class PowerBiGetAvailableFeatures extends APowerBiObjectConnector {
   protected _getCustomId = (): string => 'get-available-features';
 
-  processAction(dto: ProcessDto): Promise<ProcessDto> | ProcessDto {
+  async processAction(_dto: ProcessDto): Promise<ProcessDto> {
     const dto = _dto;
 
     const applicationInstall = await this._getApplicationInstallFromHeaders(dto);
-    const application = this._application as S3Application;
-    const client = application.getS3Client(applicationInstall);
-
-    const input: GetObjectCommandInput = {
-      [BUCKET]: this.getBucket(applicationInstall),
-      [KEY]: content[NAME],
-    };
-    const command = new GetObjectCommand(input);
+    const application = this._application as PowerBiApplication;
+    const client = application.getClient(applicationInstall);
 
     try {
-      const response: GetObjectCommandOutput = await client.send(command);
-      dto.jsonData = {
-        [NAME]: content[NAME],
-        [CONTENT]: response.Body,
-      };
+      const request = new PipelineRequest();
+      request
+        .client.sendRequest();
     } catch (e) {
       throw new OnRepeatException(60, 10, (e as Error)?.message ?? 'Unknown error.');
     }
