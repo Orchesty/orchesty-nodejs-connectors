4cc721b3b6db005912d350fc66ab3b0f9ea7a869
Jakub Sůva <suva.j@hanaboso.com>
2022-03-29 14:31:26 +0200

    Remove dialect from ASQLConnector

--- lib/Sql/Common/ASqlConnector.ts
+++ lib/Sql/Common/ASqlConnector.ts
@@ -1,17 +1,16 @@
 import ProcessDto from 'pipes-nodejs-sdk/dist/lib/Utils/ProcessDto';
 import ACommonNode from 'pipes-nodejs-sdk/dist/lib/Commons/ACommonNode';
-import { IDialect } from './ASqlApplication';
 
 export const NAME = 'sql-connector';
 
 export default abstract class ASqlConnector extends ACommonNode {
-  constructor(private _dialect: IDialect) {
-    super();
-  }
-
-  public getName = (): string => `${this._dialect}-${NAME}`;
+  protected abstract _name: string;
 
   protected abstract _processResult(res: unknown): Promise<ProcessDto> | ProcessDto;
 
   protected abstract _getQuery(): string;
+
+  public getName(): string {
+    return `${this._name}-${NAME}`;
+  }
 }
