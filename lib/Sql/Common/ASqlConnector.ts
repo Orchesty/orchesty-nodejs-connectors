9aff49976c3c229e8395ae276f42e419f9ace6a9
Jakub Sůva <suva.j@hanaboso.com>
2022-03-31 11:42:24 +0200

    Fix Abstraction

--- lib/Sql/Common/ASqlConnector.ts
+++ lib/Sql/Common/ASqlConnector.ts
@@ -1,5 +1,6 @@
 import ProcessDto from 'pipes-nodejs-sdk/dist/lib/Utils/ProcessDto';
 import ACommonNode from 'pipes-nodejs-sdk/dist/lib/Commons/ACommonNode';
+import ASqlApplication from './ASqlApplication';
 
 export const NAME = 'sql-connector';
 
@@ -8,7 +9,17 @@ export default abstract class ASqlConnector extends ACommonNode {
 
   protected abstract _processResult(res: unknown): Promise<ProcessDto> | ProcessDto;
 
-  protected abstract _getQuery(): string;
+  public async processAction(_dto: ProcessDto): Promise<ProcessDto> {
+    const dto = _dto;
+    const query = this._getQuery(dto);
+    const { userName } = dto.jsonData as { userName: string };
+    const appInstall = await this._getApplicationInstall(userName);
+    const app = this._application as ASqlApplication;
+    const result = app.getConnection(appInstall).query(query);
+    return this._processResult(result);
+  }
+
+  protected abstract _getQuery(processDto: ProcessDto): string;
 
   public getName(): string {
     return `${this._name}-${NAME}`;
