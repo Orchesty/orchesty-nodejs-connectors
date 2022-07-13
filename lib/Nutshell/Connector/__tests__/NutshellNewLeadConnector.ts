f65d6efe85dfa58ab51192bb1ee494c0ac6e16e1
Martin Musil <martin.musil@hanaboso.com>
2022-07-13 17:26:08 +0200

    Nutshell: wip

---
+++ lib/Nutshell/Connector/__tests__/NutshellNewLeadConnector.ts
@@ -0,0 +1,16 @@
+import NodeTester from '@orchesty/nodejs-sdk/dist/test/Testers/NodeTester';
+import { container } from '../../../test/TestAbstract';
+import { NAME as NUTSHELL_NEW_LEAD_CONNECTOR } from '../NutshellNewLeadConnector';
+
+let tester: NodeTester;
+
+describe('Tests for NutshellNewLeadConnector', () => {
+
+  beforeEach(async () => {
+    tester = new NodeTester(container, __filename, true);
+  });
+
+  it('process - ok', async () => {
+    await tester.testConnector(NUTSHELL_NEW_LEAD_CONNECTOR);
+  });
+});
