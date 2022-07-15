0404133d3ecac67ddd41989129f211dac50fe7ce
Tomáš Novák <tomas.novak@hanaboso.com>
2022-07-15 15:06:46 +0200

    wip

--- lib/Pipedrive/Connector/PipedriveAddLeadConnector.ts
+++ lib/Pipedrive/Connector/PipedriveAddLeadConnector.ts
@@ -1,14 +1,26 @@
 import AConnector from '@orchesty/nodejs-sdk/dist/lib/Connector/AConnector';
 import ProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/ProcessDto';
+import { AUTHORIZATION_FORM } from '@orchesty/nodejs-sdk/dist/lib/Application/Base/AApplication';
+import HttpMethods from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
+import { STORE_HASH } from '../../Bigcommerce/BigcommerceApplication';
+import { IInput, IOutput } from '../../Bigcommerce/Connector/BigcommerceCreateOrderConnector';
 
 export const NAME = 'pipedrive-add-lead-connector';
 
 export default class PipedriveAddLeadConnector extends AConnector {
-    public getName = (): string => NAME;
+  public getName = (): string => NAME;
 
-    public async processAction(_dto: ProcessDto): Promise<ProcessDto> {
-        const dto = _dto;
+  public async processAction(_dto: ProcessDto): Promise<ProcessDto> {
+    const dto = _dto;
+    const body = dto.jsonData as IInput;
 
-        return dto;
-    }
+    const appInstall = await this._getApplicationInstallFromProcess(dto);
+    const url = '/leads';
+    const req = await this._application.getRequestDto(dto, appInstall, HttpMethods.POST, url, body);
+
+    const resp = await this._sender.send(req, [200]);
+
+    dto.jsonData = resp.jsonBody as IOutput;
+    return dto;
+  }
 }
--- lib/Pipedrive/Connector/__tests__/PipedriveAddLeadConnector.ts
+++ lib/Pipedrive/Connector/__tests__/PipedriveAddLeadConnector.ts
@@ -1,16 +1,15 @@
 import NodeTester from '@orchesty/nodejs-sdk/dist/test/Testers/NodeTester';
-import {container} from '../../../test/TestAbstract';
-import {NAME as PIPEDRIVE_ADD_LEAD_CONNECTOR} from '../PipedriveAddLeadConnector';
+import { container } from '../../../test/TestAbstract';
+import { NAME as PIPEDRIVE_ADD_LEAD_CONNECTOR } from '../PipedriveAddLeadConnector';
 
 let tester: NodeTester;
 
 describe('Tests for PipedriveAddLeadConnector', () => {
+  beforeEach(async () => {
+    tester = new NodeTester(container, __filename, true);
+  });
 
-    beforeEach(async () => {
-        tester = new NodeTester(container, __filename, true);
-    });
-
-    it('process - ok', async () => {
-        await tester.testConnector(PIPEDRIVE_ADD_LEAD_CONNECTOR);
-    });
+  it('process - ok', async () => {
+    await tester.testConnector(PIPEDRIVE_ADD_LEAD_CONNECTOR);
+  });
 });
