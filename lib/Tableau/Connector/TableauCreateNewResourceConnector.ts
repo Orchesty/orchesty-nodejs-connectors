4cdfaec8ced9b08fab255320fe73936a0d7f4157
Martin Pilný <martin.pilny@box-48yup.office-czhk1.hanaboso.local>
2022-07-15 12:44:11 +0200

    wip

--- lib/Tableau/Connector/TableauCreateNewResourceConnector.ts
+++ lib/Tableau/Connector/TableauCreateNewResourceConnector.ts
@@ -1,6 +1,7 @@
 import AConnector from '@orchesty/nodejs-sdk/dist/lib/Connector/AConnector';
 import ProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/ProcessDto';
 import HttpMethods from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
+import { IRecordResponse } from '../../Zoho/Connector/ZohoAddRecordsConnector';
 
 export const NAME = 'tableau-create-new-resource-connector';
 
@@ -15,9 +16,14 @@ export default class TableauCreateNewResourceConnector extends AConnector {
       dto,
       appInstall,
       HttpMethods.POST,
+      '/item',
         dto.jsonData as IInput,
     );
     const resp = await this._sender.send(req, [200]);
+    const records = resp.jsonBody as IRecordResponse;
+
+    dto.jsonData = records.result;
+
     return dto;
   }
 }
