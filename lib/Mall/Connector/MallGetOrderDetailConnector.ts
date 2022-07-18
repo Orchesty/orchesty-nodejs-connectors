9707a2b0f3ccb4fc6730edd0ef283985e8baef05
Martin Musil <martin.musil@hanaboso.com>
2022-07-18 11:13:31 +0200

    Mall: Create get order detail

--- lib/Mall/Connector/MallGetOrderDetailConnector.ts
+++ lib/Mall/Connector/MallGetOrderDetailConnector.ts
@@ -29,11 +29,11 @@ export default class MallGetOrderDetailConnector extends AConnector {
 /* eslint-disable @typescript-eslint/naming-convention */
 
 interface IResponse{
-  'result': {
-    'code': number,
-    'status': string
+  result: {
+    code: number,
+    status: string
   },
-  'data': IOutput[]
+  data: IOutput[]
 }
 
 export interface IInput{
@@ -41,78 +41,78 @@ export interface IInput{
 }
 
 export interface IOutput{
-  'id': number,
-  'purchase_id': number,
-  'cod': number,
-  'ship_date': string,
-  'delivered_at': string,
-  'status': string,
-  'confirmed': boolean,
-  'test': boolean,
-  'mdp': boolean,
-  'mdp_classic': boolean,
-  'mdp_spectrum': boolean,
-  'shipped': string,
-  'shipping': string,
-  'open': string,
-  'blocked': string,
-  'lost': string,
-  'returned': string,
-  'cancelled': string,
-  'delivered': string,
-  'external_order_id': number,
-  'currency': string,
-  'delivery_price': number,
-  'cod_price': number,
-  'discount': number,
-  'delivery_method': string,
-  'delivery_method_id': number,
-  'branch_id': number,
-  'branches': {
-    'branch_id': number,
-    'secondary_branch_id': number,
-    'overridden': boolean,
-    'last_change': string
+  id: number,
+  purchase_id: number,
+  cod: number,
+  ship_date: string,
+  delivered_at: string,
+  status: string,
+  confirmed: boolean,
+  test: boolean,
+  mdp: boolean,
+  mdp_classic: boolean,
+  mdp_spectrum: boolean,
+  shipped: string,
+  shipping: string,
+  open: string,
+  blocked: string,
+  lost: string,
+  returned: string,
+  cancelled: string,
+  delivered: string,
+  external_order_id: number,
+  currency: string,
+  delivery_price: number,
+  cod_price: number,
+  discount: number,
+  delivery_method: string,
+  delivery_method_id: number,
+  branch_id: number,
+  branches: {
+    branch_id: number,
+    secondary_branch_id: number,
+    overridden: boolean,
+    last_change: string
   },
-  'tracking_number': string,
-  'ready_to_return': boolean,
-  'address': {
-    'name': string,
-    'company': string,
-    'phone': number,
-    'email': string,
-    'street': string,
-    'city': string,
-    'zip': number,
-    'country': string,
-    'customer_id': number
+  tracking_number: string,
+  ready_to_return: boolean,
+  address: {
+    name: string,
+    company: string,
+    phone: number,
+    email: string,
+    street: string,
+    city: string,
+    zip: number,
+    country: string,
+    customer_id: number
   },
-  'items': [
+  items: [
     {
-      'id': string,
-      'article_id': number,
-      'title': string,
-      'quantity': number,
-      'price': number,
-      'purchase_price': number,
-      'vat': number,
-      'commission': number,
-      'serial_numbers': string[]
+      id: string,
+      article_id: number,
+      title: string,
+      quantity: number,
+      price: number,
+      purchase_price: number,
+      vat: number,
+      commission: number,
+      serial_numbers: string[]
     }
   ],
-  'ulozenka_status_history': [
+  ulozenka_status_history: [
     {
-      'id': number,
-      'name': string,
-      'date': string
+      id: number,
+      name: string,
+      date: string
     }
   ],
-  'consignment_status_history': [
+  consignment_status_history: [
     {
-      'code': string,
-      'date': string,
-      'flags': string[],
-      'tracking_number': string
+      code: string,
+      date: string,
+      flags: string[],
+      tracking_number: string
     }
   ]
 }
