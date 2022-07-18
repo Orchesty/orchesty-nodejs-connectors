be8e7ea925ed800c8bf91bee484c1c0f6e5506ca
Tomáš Novák <tomas.novak@hanaboso.com>
2022-07-18 14:01:06 +0200

    Allegro: application

--- lib/Allegro/AllegroApplication.ts
+++ lib/Allegro/AllegroApplication.ts
@@ -3,6 +3,7 @@ import Form from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/Form';
 import FormStack from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/FormStack';
 import HttpMethods from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
 import RequestDto from '@orchesty/nodejs-sdk/dist/lib/Transport/Curl/RequestDto';
+import { Headers } from 'node-fetch';
 import { ApplicationInstall } from '@orchesty/nodejs-sdk/dist/lib/Application/Database/ApplicationInstall';
 import { AUTHORIZATION_FORM } from '@orchesty/nodejs-sdk/dist/lib/Application/Base/AApplication';
 import { CommonHeaders, JSON_TYPE } from '@orchesty/nodejs-sdk/dist/lib/Utils/Headers';
@@ -10,43 +11,34 @@ import AOAuth2Application from '@orchesty/nodejs-sdk/dist/lib/Authorization/Type
 import Field from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/Field';
 import FieldType from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/FieldType';
 import { CLIENT_ID, CLIENT_SECRET } from '@orchesty/nodejs-sdk/dist/lib/Authorization/Type/OAuth2/IOAuth2Application';
-import { STORE_HASH } from '../Bigcommerce/BigcommerceApplication';
+
+export const NAME = 'allegro';
 
 export default class AllegroApplication extends AOAuth2Application {
-  public getName(): string {
-    return 'allegro';
-  }
+  public getName = (): string => NAME;
 
-  public getPublicName(): string {
-    return 'Allegro';
-  }
+  public getPublicName = (): string => 'Allegro';
 
-  public getDescription(): string {
-    return 'Allegro description';
-  }
+  public getDescription = (): string => 'Allegro description';
 
   public getAuthUrl = (): string => 'https://allegro.pl/auth/oauth/authorize';
 
   public getTokenUrl = (): string => 'https://allegro.pl/auth/oauth/token';
 
-  public getFormStack(): FormStack {
-    const form = new Form(AUTHORIZATION_FORM, 'Authorization settings');
-
-    return new FormStack().addForm(form);
-  }
-
   public getRequestDto(
     dto: AProcessDto,
     applicationInstall: ApplicationInstall,
     method: HttpMethods,
-    url?: string,
+    _url?: string,
     data?: unknown,
-  ): RequestDto {
-    const request = new RequestDto(url ?? '', method, dto);
-    request.headers = {
+  ): RequestDto | Promise<RequestDto> {
+    const url = `/${_url}`;
+    const request = new RequestDto(url, method, dto);
+    request.headers = new Headers({
       [CommonHeaders.CONTENT_TYPE]: JSON_TYPE,
       [CommonHeaders.ACCEPT]: JSON_TYPE,
-    };
+      [CommonHeaders.AUTHORIZATION]: `Bearer ${this.getAccessToken(applicationInstall)}`,
+    });
 
     if (data) {
       request.setJsonBody(data);
@@ -58,12 +50,12 @@ export default class AllegroApplication extends AOAuth2Application {
   public getFormStack = (): FormStack => {
     const form = new Form(AUTHORIZATION_FORM, 'Authorization settings')
       .addField(new Field(FieldType.TEXT, CLIENT_ID, 'Client Id', null, true))
-      .addField(new Field(FieldType.TEXT, CLIENT_SECRET, 'Client Secret', null, true))
-      .addField(new Field(FieldType.TEXT, STORE_HASH, 'Store hash', null, true));
+      .addField(new Field(FieldType.TEXT, CLIENT_SECRET, 'Client Secret', null, true));
 
     return new FormStack()
       .addForm(form);
   };
 
-  // public getScopes = (applicationInstall: ApplicationInstall): string[] => ['store_v2_products'];
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public getScopes = (applicationInstall: ApplicationInstall): string[] => [];
 }
