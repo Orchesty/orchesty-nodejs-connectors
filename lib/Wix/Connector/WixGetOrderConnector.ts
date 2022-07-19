789309a5e588d37d858ca41cc1d6ed7c515ec65f
Martin Musil <martin.musil@hanaboso.com>
2022-07-19 11:12:21 +0200

    Wix: fix bug

--- lib/Wix/Connector/WixGetOrderConnector.ts
+++ lib/Wix/Connector/WixGetOrderConnector.ts
@@ -31,106 +31,106 @@ export interface IInput{
 }
 
 export interface IOutput{
-  'order': {
-    'id': string,
-    'number': number,
-    'dateCreated': string,
-    'currency': string,
-    'weightUnit': string,
-    'totals': {
-      'subtotal': string,
-      'shipping': string,
-      'tax': string,
-      'discount': string,
-      'total': string,
-      'weight': string,
-      'quantity': number
+  order: {
+    id: string,
+    number: number,
+    dateCreated: string,
+    currency: string,
+    weightUnit: string,
+    totals: {
+      subtotal: string,
+      shipping: string,
+      tax: string,
+      discount: string,
+      total: string,
+      weight: string,
+      quantity: number
     },
-    'billingInfo': {
-      'paymentMethod': string,
-      'externalTransactionId': string,
-      'paymentProviderTransactionId': string,
-      'address': {
-        'fullName': {
-          'firstName': string,
-          'lastName': string
+    billingInfo: {
+      paymentMethod: string,
+      externalTransactionId: string,
+      paymentProviderTransactionId: string,
+      address: {
+        fullName: {
+          firstName: string,
+          lastName: string
         },
-        'country': string,
-        'city': string,
-        'zipCode': string,
-        'phone': string,
-        'email': string
+        country: string,
+        city: string,
+        zipCode: string,
+        phone: string,
+        email: string
       },
-      'paidDate': string
+      paidDate: string
     },
-    'shippingInfo': {
-      'deliveryOption': string,
-      'shippingRegion': string,
-      'estimatedDeliveryTime': string,
-      'shipmentDetails': {
-        'address': {
-          'fullName': {
-            'firstName': string,
-            'lastName': string
+    shippingInfo: {
+      deliveryOption: string,
+      shippingRegion: string,
+      estimatedDeliveryTime: string,
+      shipmentDetails: {
+        address: {
+          fullName: {
+            firstName: string,
+            lastName: string
           },
-          'country': string,
-          'city': string,
-          'zipCode': string,
-          'phone': string,
-          'email': string
+          country: string,
+          city: string,
+          zipCode: string,
+          phone: string,
+          email: string
         },
-        'discount': string,
-        'tax': string,
-        'priceData': {
-          'taxIncludedInPrice': boolean,
-          'price': string
+        discount: string,
+        tax: string,
+        priceData: {
+          taxIncludedInPrice: boolean,
+          price: string
         }
       }
     },
-    'read': boolean,
-    'archived': boolean,
-    'paymentStatus': string,
-    'fulfillmentStatus': string,
-    'lineItems': [ {
-      'index': number,
-      'quantity': number,
-      'price': string,
-      'name': string,
-      'productId': string,
-      'totalPrice': string,
-      'lineItemType': string,
-      'options': [],
-      'customTextFields': [],
-      'weight': string,
-      'sku': string,
-      'discount': string,
-      'tax': string,
-      'taxIncludedInPrice': boolean,
-      'priceData': {
-        'taxIncludedInPrice': boolean,
-        'price': string,
-        'totalPrice': string
+    read: boolean,
+    archived: boolean,
+    paymentStatus: string,
+    fulfillmentStatus: string,
+    lineItems: [ {
+      index: number,
+      quantity: number,
+      price: string,
+      name: string,
+      productId: string,
+      totalPrice: string,
+      lineItemType: string,
+      options: [],
+      customTextFields: [],
+      weight: string,
+      sku: string,
+      discount: string,
+      tax: string,
+      taxIncludedInPrice: boolean,
+      priceData: {
+        taxIncludedInPrice: boolean,
+        price: string,
+        totalPrice: string
       }
     }],
-    'activities': [
+    activities: [
       {
-        'type': string,
-        'timestamp': string
+        type: string,
+        timestamp: string
       },
       {
-        'type': string,
-        'timestamp': string
+        type: string,
+        timestamp: string
       }
     ],
-    'fulfillments': [],
-    'discount': {'value': string},
-    'buyerLanguage': string,
-    'channelInfo': {'type': string},
-    'enteredBy': {
-      'id': string,
-      'identityType': string
+    fulfillments: [],
+    discount: {value: string},
+    buyerLanguage: string,
+    channelInfo: {type: string},
+    enteredBy: {
+      id: string,
+      identityType: string
     },
-    'lastUpdated': string
+    lastUpdated: string
   }
 }
 /* eslint-enable @typescript-eslint/naming-convention */
