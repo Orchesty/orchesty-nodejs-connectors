0f9a7e3165ba091c7424d7a454765a4d48d22174
Martin Musil <martin.musil@hanaboso.com>
2022-07-20 10:24:51 +0200

    fix bug

--- lib/Alza/Connectors/AlzaCancelOrderConnector.ts
+++ lib/Alza/Connectors/AlzaCancelOrderConnector.ts
@@ -9,7 +9,7 @@ export default class AlzaCancelOrderConnector extends AConnector {
 
   public async processAction(_dto: ProcessDto): Promise<ProcessDto> {
     const dto = _dto;
-    const { cancelOrder, ...body } = dto.jsonData as IInput;
+    const { cancelOrder } = dto.jsonData as IInput;
 
     const appInstall = await this._getApplicationInstallFromProcess(dto);
     const req = await this._application.getRequestDto(
@@ -17,7 +17,6 @@ export default class AlzaCancelOrderConnector extends AConnector {
       appInstall,
       HttpMethods.DELETE,
       `order/${cancelOrder}`,
-      body,
     );
     const resp = await this._sender.send(req, [200]);
     dto.jsonData = resp.jsonBody as IOutput;
@@ -28,12 +27,6 @@ export default class AlzaCancelOrderConnector extends AConnector {
 
 export interface IInput{
   cancelOrder: string
-  timestamp: string,
-  customerId: number,
-  supplierId: number,
-  supplierBranchId: number,
-  regNo: string,
-  vatNo: string
 }
 
 export interface IOutput{
