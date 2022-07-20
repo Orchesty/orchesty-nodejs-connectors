90d74b2ecec763748b59da6ba2eeec14d9314cea
Martin Pilný <martin.pilny@hanaboso.com>
2022-07-20 12:15:48 +0200

    Fakturaonline: wip

--- lib/Fakturaonline/FakturaonlineApplication.ts
+++ lib/Fakturaonline/FakturaonlineApplication.ts
@@ -3,7 +3,6 @@ import Form from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/Form';
 import FormStack from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/FormStack';
 import HttpMethods from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
 import RequestDto from '@orchesty/nodejs-sdk/dist/lib/Transport/Curl/RequestDto';
-import ResponseDto from '@orchesty/nodejs-sdk/dist/lib/Transport/Curl/ResponseDto';
 import { ABasicApplication } from '@orchesty/nodejs-sdk/dist/lib/Authorization/Type/Basic/ABasicApplication';
 import { ApplicationInstall } from '@orchesty/nodejs-sdk/dist/lib/Application/Database/ApplicationInstall';
 import { AUTHORIZATION_FORM } from '@orchesty/nodejs-sdk/dist/lib/Application/Base/AApplication';
@@ -14,28 +13,25 @@ export const NAME = 'fakturaonline';
 export default class FakturaonlineApplication extends ABasicApplication {
   public getName = (): string => NAME;
 
-  public getPublicName(): string {
-    return 'Fakturaonline';
-  }
+  public getPublicName = (): string => 'Fakturaonline';
 
-  public getDescription(): string {
-    return 'Fakturaonline description';
-  }
+  public getDescription = (): string => 'Fakturaonline description';
 
-  public getFormStack(): FormStack {
+  public getFormStack = (): FormStack => {
     const form = new Form(AUTHORIZATION_FORM, 'Authorization settings');
 
     return new FormStack().addForm(form);
-  }
+  };
 
-  public getRequestDto(
+  public getRequestDto = (
     dto: AProcessDto,
     applicationInstall: ApplicationInstall,
     method: HttpMethods,
-    url?: string,
+    _url?: string,
     data?: unknown,
-  ): RequestDto {
-    const request = new RequestDto(url ?? '', method, dto);
+  ): RequestDto => {
+    const url = `https://api.fakturaonline.cz/v0/${_url}`;
+    const request = new RequestDto(url, method, dto);
     request.headers = {
       [CommonHeaders.CONTENT_TYPE]: JSON_TYPE,
       [CommonHeaders.ACCEPT]: JSON_TYPE,
@@ -46,5 +42,5 @@ export default class FakturaonlineApplication extends ABasicApplication {
     }
 
     return request;
-  }
+  };
 }
