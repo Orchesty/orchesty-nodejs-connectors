ba08f1f31e0659bec668a9693273c784a2ae6a0d
Tomáš Novák <tomas.novak@hanaboso.com>
2022-07-21 11:00:36 +0200

    Allegro: getAvailableProducts Batch

--- lib/Allegro/Batch/AllegroGetAvailableProductsBatch.ts
+++ lib/Allegro/Batch/AllegroGetAvailableProductsBatch.ts
@@ -12,7 +12,7 @@ export default class AllegroGetAvailableProductsBatch extends ABatchNode {
     const dto = _dto;
     const offset = Number(dto.getBatchCursor('0'));
     const appInstall = await this._getApplicationInstallFromProcess(dto);
-    const url = `order/checkout-forms?offset=${(offset * LIMIT) + offset ? 1 : 0}&limit=${LIMIT}`;
+    const url = `fulfillment/available-products?offset=${offset}&limit=${LIMIT}`;
     const req = await this._application.getRequestDto(
       dto,
       appInstall,
@@ -22,18 +22,29 @@ export default class AllegroGetAvailableProductsBatch extends ABatchNode {
     const resp = await this._sender.send(req, [200]);
     const response = resp.jsonBody as IResponse;
 
-    dto.setItemList(response.checkoutForms ?? []);
+    dto.setItemList(response.products ?? []);
     if (response.count === LIMIT + 1) {
-      dto.setBatchCursor((offset + 1).toString());
+      dto.setBatchCursor((offset + LIMIT).toString());
     }
     return dto;
   }
 }
 
+/* eslint-disable @typescript-eslint/naming-convention */
+
 interface IResponse {
     products: IOutput[];
-
+    count: number;
+    totalCount: number;
 }
+
 export interface IOutput {
-
+    products: [{
+        id: string;
+        name: string;
+        gtins: string[];
+        image: string;
+    }];
 }
+
+/* eslint-enable @typescript-eslint/naming-convention */
