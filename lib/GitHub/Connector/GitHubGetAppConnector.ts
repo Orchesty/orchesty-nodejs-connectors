ec2afa44bd25269ed93c59c3a161e7df1d0e7ac4
Martin Musil <martin.musil@hanaboso.com>
2022-07-22 09:44:19 +0200

    fix bug

--- lib/GitHub/Connector/GitHubGetAppConnector.ts
+++ lib/GitHub/Connector/GitHubGetAppConnector.ts
@@ -32,41 +32,41 @@ export interface IInput {
 }
 
 export interface IOutput{
-  'id': number,
-  'slug': string,
-  'node_id': string,
-  'owner': {
-    'login': string,
-    'id': number,
-    'node_id': string,
-    'url': string,
-    'repos_url': string,
-    'events_url': string,
-    'avatar_url': string,
-    'gravatar_id': string,
-    'html_url': string,
-    'followers_url': string,
-    'following_url': string,
-    'gists_url': string,
-    'starred_url': string,
-    'subscriptions_url': string,
-    'organizations_url': string,
-    'received_events_url': string,
-    'type': string,
-    'site_admin': boolean
+  id: number,
+  slug: string,
+  node_id: string,
+  owner: {
+    login: string,
+    id: number,
+    node_id: string,
+    url: string,
+    repos_url: string,
+    events_url: string,
+    avatar_url: string,
+    gravatar_id: string,
+    html_url: string,
+    followers_url: string,
+    following_url: string,
+    gists_url: string,
+    starred_url: string,
+    subscriptions_url: string,
+    organizations_url: string,
+    received_events_url: string,
+    type: string,
+    site_admin: boolean
   },
-  'name': string,
-  'description': string,
-  'external_url': string,
-  'html_url': string,
-  'created_at': string,
-  'updated_at': string,
-  'permissions': {
-    'metadata': string,
-    'contents': string,
-    'issues': string,
-    'single_file': string
+  name: string,
+  description: string,
+  external_url: string,
+  html_url: string,
+  created_at: string,
+  updated_at: string,
+  permissions: {
+    metadata: string,
+    contents: string,
+    issues: string,
+    single_file: string
   },
-  'events': string[]
+  events: string[]
 }
 /* eslint-enable @typescript-eslint/naming-convention */
