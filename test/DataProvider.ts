fb22da7b6165e852eb9cdf68613b14bfec947f5c
Martin Musil <martin.musil@hanaboso.com>
2022-07-22 09:30:54 +0200

    GitHub: Created get app

--- test/DataProvider.ts
+++ test/DataProvider.ts
@@ -34,6 +34,7 @@ import {
 import { ENVIRONMENT, NAME as ALLEGRO_APP } from '../lib/Allegro/AllegroApplication';
 import { NAME as WEDO_APP } from '../lib/Wedo/WedoApplication';
 import { NAME as MERGADO_APP } from '../lib/Mergado/MergadoApplication';
+import { NAME as GITHUB_APP } from '../lib/GitHub/GitHubApplication';
 import { NAME as VYFAKTURUJ_APP, USER_EMAIL } from '../lib/Vyfakturuj/VyfakturujApplication';
 
 const DEFAULT_USER = 'TestUser';
@@ -250,6 +251,15 @@ export async function mergadoApp() {
       [TOKEN]: {
         [ACCESS_TOKEN]: DEFAULT_ACCESS_TOKEN,
       },
+    },
+  });
+}
+
+export async function gitHubApp() {
+  return appInstall(GITHUB_APP, DEFAULT_USER, {
+    [AUTHORIZATION_FORM]: {
+      [USER]: DEFAULT_USER,
+      [TOKEN]: DEFAULT_ACCESS_TOKEN,
     },
   });
 }
