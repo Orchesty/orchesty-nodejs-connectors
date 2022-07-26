ea37c70f4c5411d527e43d3cf01b9d5a9bbe3f71
Martin Pilný <martin.pilny@hanaboso.com>
2022-07-26 10:19:25 +0200

    created input, mock, output

--- lib/Fakturaonline/Connector/__tests__/FakturaonlineUpdateInvoiceConnector.ts
+++ lib/Fakturaonline/Connector/__tests__/FakturaonlineUpdateInvoiceConnector.ts
@@ -1,12 +1,14 @@
 import NodeTester from '@orchesty/nodejs-sdk/dist/test/Testers/NodeTester';
-import { container } from '../../../test/TestAbstract';
+import { container } from '../../../../test/TestAbstract';
 import { NAME as FAKTURAONLINE_UPDATE_INVOICE_CONNECTOR } from '../FakturaonlineUpdateInvoiceConnector';
+import { fakturaonlineApp } from '../../../../test/DataProvider';
 
 let tester: NodeTester;
 
 describe('Tests for FakturaonlineUpdateInvoiceConnector', () => {
   beforeEach(async () => {
     tester = new NodeTester(container, __filename, true);
+    await fakturaonlineApp();
   });
 
   it('process - ok', async () => {
