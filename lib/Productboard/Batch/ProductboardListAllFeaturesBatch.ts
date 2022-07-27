4d2e219aaf69605f7c9656e0c2783da4d955aff5
Tomáš Novák <tomas.novak@hanaboso.com>
2022-07-27 13:48:55 +0200

    Productboard: listAllFeatures Batch

--- lib/Productboard/Batch/ProductboardListAllFeaturesBatch.ts
+++ lib/Productboard/Batch/ProductboardListAllFeaturesBatch.ts
@@ -1,14 +1,69 @@
 import ABatchNode from '@orchesty/nodejs-sdk/dist/lib/Batch/ABatchNode';
 import BatchProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/BatchProcessDto';
+import HttpMethods from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
 
 export const NAME = 'productboard-list-all-features-batch';
-
+const LIMIT = 99;
 export default class ProductboardListAllFeaturesBatch extends ABatchNode {
-    public getName = (): string => NAME;
+  public getName = (): string => NAME;
 
-    public async processAction(_dto: BatchProcessDto): Promise<BatchProcessDto> {
-        const dto = _dto;
+  public async processAction(_dto: BatchProcessDto): Promise<BatchProcessDto> {
+    const dto = _dto;
+    const offset = Number(dto.getBatchCursor('0'));
+    const appInstall = await this._getApplicationInstallFromProcess(dto);
+    const url = `features?pageLimit=${LIMIT}&pageOffset=${offset}`;
+    const req = await this._application.getRequestDto(
+      dto,
+      appInstall,
+      HttpMethods.GET,
+      url,
+    );
+    const resp = await this._sender.send(req, [200]);
+    const response = resp.jsonBody as IResponse;
 
-        return dto;
+    dto.setItemList(response.data ?? []);
+    if (response.links.next) {
+      dto.setBatchCursor((offset + LIMIT).toString());
     }
+    return dto;
+  }
+}
+
+interface IResponse {
+    data: IOutput[];
+    links: {
+        next: string;
+    }
+}
+
+export interface IOutput {
+    id: string;
+    name: string;
+    description: string;
+    type: string;
+    archived: boolean;
+    status: {
+        id: string;
+        name: string;
+    };
+    parent: {
+        feature: {
+            id: string;
+            links: {
+                self: string;
+            };
+        };
+    };
+    links: {
+        self: string;
+        html: string;
+    };
+    timeframe: {
+        startDate: string;
+        endDate: string;
+        granularity: string;
+    };
+    owner: {
+        email: string;
+    };
 }
--- lib/Productboard/Batch/__tests__/ProductboardListAllFeaturesBatch.ts
+++ lib/Productboard/Batch/__tests__/ProductboardListAllFeaturesBatch.ts
@@ -1,12 +1,14 @@
 import NodeTester from '@orchesty/nodejs-sdk/dist/test/Testers/NodeTester';
-import { container } from '../../../test/TestAbstract';
+import { container } from '../../../../test/TestAbstract';
 import { NAME as PRODUCTBOARD_LIST_ALL_FEATURES_BATCH } from '../ProductboardListAllFeaturesBatch';
+import { productboardApp } from '../../../../test/DataProvider';
 
 let tester: NodeTester;
 
 describe('Tests for ProductboardListAllFeaturesBatch', () => {
   beforeEach(async () => {
     tester = new NodeTester(container, __filename);
+    await productboardApp();
   });
 
   it('process - ok', async () => {
