b2293bb4d646c5b6f80549ce8cf0e2dd3fd31f85
Martin Pilný <martin.pilny@hanaboso.com>
2022-08-01 10:12:53 +0200

    fixed

--- lib/GObalik/Connectors/__tests__/GObalikOrdeDeatilConnector.ts
+++ lib/GObalik/Connectors/__tests__/GObalikOrdeDeatilConnector.ts
@@ -1,17 +1,17 @@
 import NodeTester from '@orchesty/nodejs-sdk/dist/test/Testers/NodeTester';
 import { container } from '../../../../test/TestAbstract';
-import { NAME as GO_BALIK_ORDE_DEATIL_CONNECTOR } from '../GObalikOrderDeatilConnector';
+import { NAME as GO_BALIK_ORDER_DEATIL_CONNECTOR } from '../GObalikOrderDeatilConnector';
 import { gobalikApp } from '../../../../test/DataProvider';
 
 let tester: NodeTester;
 
-describe('Tests for GObalikOrderDeatilConnector', () => {
+describe('Tests for GObalikOrdeDeatilConnector', () => {
   beforeEach(async () => {
     tester = new NodeTester(container, __filename, true);
     await gobalikApp();
   });
 
   it('process - ok', async () => {
-    await tester.testConnector(GO_BALIK_ORDE_DEATIL_CONNECTOR);
+    await tester.testConnector(GO_BALIK_ORDER_DEATIL_CONNECTOR);
   });
 });
