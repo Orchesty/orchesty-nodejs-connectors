81f98d8260153c4b2d524139f7a50e2ca35c50df
Tomáš Novák <tomas.novak@hanaboso.com>
2022-08-10 14:32:41 +0200

    Sendinblue: sendEmail connector

---
+++ lib/Sendinblue/Connector/SendinblueSendEmailConnector.ts
@@ -0,0 +1,14 @@
+import AConnector from '@orchesty/nodejs-sdk/dist/lib/Connector/AConnector';
+import ProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/ProcessDto';
+
+export const NAME = 'sendinblue-send-email-connector';
+
+export default class SendinblueSendEmailConnector extends AConnector {
+    public getName = (): string => NAME;
+
+    public async processAction(_dto: ProcessDto): Promise<ProcessDto> {
+        const dto = _dto;
+
+        return dto;
+    }
+}
---
+++ lib/Sendinblue/SendinblueApplication.ts
@@ -0,0 +1,50 @@
+import AProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/AProcessDto';
+import Form from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/Form';
+import FormStack from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/FormStack';
+import HttpMethods from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
+import RequestDto from '@orchesty/nodejs-sdk/dist/lib/Transport/Curl/RequestDto';
+import { ABasicApplication } from '@orchesty/nodejs-sdk/dist/lib/Authorization/Type/Basic/ABasicApplication';
+import { ApplicationInstall } from '@orchesty/nodejs-sdk/dist/lib/Application/Database/ApplicationInstall';
+import { AUTHORIZATION_FORM } from '@orchesty/nodejs-sdk/dist/lib/Application/Base/AApplication';
+import { CommonHeaders, JSON_TYPE } from '@orchesty/nodejs-sdk/dist/lib/Utils/Headers';
+import Field from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/Field';
+import FieldType from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/FieldType';
+
+export const NAME = 'sendinblue';
+export const API_KEY = 'api_key';
+
+export default class SendinblueApplication extends ABasicApplication {
+  public getName = (): string => NAME;
+
+  public getPublicName = (): string => 'Sendinblue';
+
+  public getDescription = (): string => 'Sendinblue description';
+
+  public getRequestDto = (
+    dto: AProcessDto,
+    applicationInstall: ApplicationInstall,
+    method: HttpMethods,
+    url?: string,
+    data?: unknown,
+  ): RequestDto => {
+    const request = new RequestDto(url ?? '', method, dto);
+    request.headers = {
+      [CommonHeaders.CONTENT_TYPE]: JSON_TYPE,
+      [CommonHeaders.ACCEPT]: JSON_TYPE,
+      'api-key': applicationInstall.getSettings()[AUTHORIZATION_FORM][API_KEY],
+    };
+
+    if (data) {
+      request.setJsonBody(data);
+    }
+
+    return request;
+  };
+
+  public getFormStack = (): FormStack => {
+    const form = new Form(AUTHORIZATION_FORM, 'Authorization settings')
+      .addField(new Field(FieldType.TEXT, API_KEY, 'API key', undefined, true));
+
+    return new FormStack().addForm(form);
+  };
+}
