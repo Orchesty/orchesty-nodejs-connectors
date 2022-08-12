50d5d9cbc679628da963b1e4ef36a6182f307a49
Richard Fíla <fila.r@hanaboso.com>
2022-08-12 08:51:15 +0000

    Aut 35

---
+++ test/Implementation/woocommerce.ts
@@ -0,0 +1,42 @@
+import { AUTHORIZATION_FORM } from '@orchesty/nodejs-sdk/dist/lib/Application/Base/AApplication';
+import { PASSWORD, USER } from '@orchesty/nodejs-sdk/dist/lib/Authorization/Type/Basic/ABasicApplication';
+import {
+  appInstall,
+  DEFAULT_PASSWORD,
+  DEFAULT_USER,
+} from '../DataProvider';
+import {
+  container, db, sender,
+} from '../TestAbstract';
+import WooCommerceGetProducts from '../../lib/WooCommerce/Batch/WooCommerceGetProducts';
+import WooCommerceApplication, { NAME, WOOCOMMERCE_URL } from '../../lib/WooCommerce/WooCommerceApplication';
+import WooCommerceGetVariants from '../../lib/WooCommerce/Batch/WooCommerceGetVariants';
+
+export default async function init(): Promise<void> {
+  await appInstall(
+    NAME,
+    DEFAULT_USER,
+    {
+      [AUTHORIZATION_FORM]: {
+        [USER]: DEFAULT_USER,
+        [PASSWORD]: DEFAULT_PASSWORD,
+        [WOOCOMMERCE_URL]: 'http://woocomerce.com',
+      },
+    },
+  );
+
+  const app = new WooCommerceApplication();
+  container.setApplication(app);
+
+  const wooCommerceGetProducts = new WooCommerceGetProducts()
+    .setSender(sender)
+    .setDb(db)
+    .setApplication(app);
+  container.setBatch(wooCommerceGetProducts);
+
+  const wooCommerceGetVariants = new WooCommerceGetVariants()
+    .setSender(sender)
+    .setDb(db)
+    .setApplication(app);
+  container.setBatch(wooCommerceGetVariants);
+}
