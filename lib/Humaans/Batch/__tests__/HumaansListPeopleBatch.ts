449e6fba1808feb6a648fb988907181cc73dbeeb
Martin Pilný <martin.pilny@hanaboso.com>
2022-08-16 09:46:31 +0200

    wip

--- lib/Humaans/Batch/__tests__/HumaansListPeopleBatch.ts
+++ lib/Humaans/Batch/__tests__/HumaansListPeopleBatch.ts
@@ -7,11 +7,11 @@ let tester: NodeTester;
 
 describe('Tests for HumaansListPeopleBatch', () => {
   beforeEach(async () => {
-    tester = new NodeTester(container, __filename);
+    tester = new NodeTester(container, __filename, false, ['']);
     await init();
   });
 
   it('process - ok', async () => {
-    await tester.testBatch(HUMAANS_LIST_PEOPLE_BATCH);
+    await tester.testBatch(HUMAANS_LIST_PEOPLE_BATCH, '');
   });
 });
