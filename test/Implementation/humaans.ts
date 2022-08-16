21f11ea4d29d35fb2e2b5dde4b3a71072f159b64
Martin Pilný <martin.pilny@hanaboso.com>
2022-08-16 10:55:29 +0200

    created test list people batch

--- test/Implementation/humaans.ts
+++ test/Implementation/humaans.ts
@@ -1,5 +1,5 @@
 import { AUTHORIZATION_FORM } from '@orchesty/nodejs-sdk/dist/lib/Application/Base/AApplication';
-import { appInstall, DEFAULT_USER } from '../DataProvider';
+import { appInstall, DEFAULT_ACCESS_TOKEN, DEFAULT_USER } from '../DataProvider';
 import HumaansApplication, { NAME as HUMAANS_APP, APPLICATION_TOKEN } from '../../lib/Humaans/HumaansApplication';
 import { container, db, sender } from '../TestAbstract';
 import HumaansListCompaniesBatch from '../../lib/Humaans/Batch/HumaansListCompaniesBatch';
@@ -8,7 +8,7 @@ import HumaansListPeopleBatch from '../../lib/Humaans/Batch/HumaansListPeopleBat
 export default async function init(): Promise<void> {
   await appInstall(HUMAANS_APP, DEFAULT_USER, {
     [AUTHORIZATION_FORM]: {
-      [APPLICATION_TOKEN]: 'gX67NyrVhuF81TEI7evS3WaYSpwO9UqA',
+      [APPLICATION_TOKEN]: DEFAULT_ACCESS_TOKEN,
     },
   });
   const app = new HumaansApplication();
