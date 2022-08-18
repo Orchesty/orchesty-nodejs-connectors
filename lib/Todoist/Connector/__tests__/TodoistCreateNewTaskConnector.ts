8acc84df40bcad8fa2cddb152f6ba84e45ea5c6f
Martin Musil <martin.musil05@gmail.com>
2022-08-18 13:53:50 +0000

    Todoist: Created tests

--- lib/Todoist/Connector/__tests__/TodoistCreateNewTaskConnector.ts
+++ lib/Todoist/Connector/__tests__/TodoistCreateNewTaskConnector.ts
@@ -1,14 +1,14 @@
 import NodeTester from '@orchesty/nodejs-sdk/dist/test/Testers/NodeTester';
 import { container } from '../../../../test/TestAbstract';
 import { NAME as TODOIST_CREATE_NEW_TASK_CONNECTOR } from '../TodoistCreateNewTaskConnector';
-import { todoistApp } from '../../../../test/DataProvider';
+import init from '../../../../test/Implementation/todoist';
 
 let tester: NodeTester;
 
 describe('Tests for TodoistCreateNewTaskConnector', () => {
   beforeEach(async () => {
-    tester = new NodeTester(container, __filename, true);
-    await todoistApp();
+    tester = new NodeTester(container, __filename);
+    await init();
   });
 
   it('process - ok', async () => {
