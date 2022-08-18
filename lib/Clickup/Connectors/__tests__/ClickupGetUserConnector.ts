f208a7e747439ba534ef7ccef6f1594fb5c5d432
Martin Musil <martin.musil05@gmail.com>
2022-08-18 12:08:42 +0000

    Clickup: Created tests

--- lib/Clickup/Connectors/__tests__/ClickupGetUserConnector.ts
+++ lib/Clickup/Connectors/__tests__/ClickupGetUserConnector.ts
@@ -7,7 +7,7 @@ let tester: NodeTester;
 
 describe('Tests for ClickupGetUserConnector', () => {
   beforeEach(async () => {
-    tester = new NodeTester(container, __filename, true);
+    tester = new NodeTester(container, __filename);
     await clickupApp();
   });
 
