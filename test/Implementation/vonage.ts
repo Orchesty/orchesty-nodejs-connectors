8f72b3a23066ed95f3d6842adfa98bc0a7c191f1
Martin Musil <martin.musil05@gmail.com>
2022-08-18 12:08:04 +0000

    Vonage: Created connector + app

---
+++ test/Implementation/vonage.ts
@@ -0,0 +1,26 @@
+import { AUTHORIZATION_FORM } from '@orchesty/nodejs-sdk/dist/lib/Application/Base/AApplication';
+import { appInstall, DEFAULT_USER } from '../DataProvider';
+import { container, db, sender } from '../TestAbstract';
+
+import VonageApplication, { API_KEY, API_SECRET, NAME } from '../../lib/Vonage /VonageApplication';
+import VonageSendSMSConnector from '../../lib/Vonage /Connector/VonageSendSMSConnector';
+
+export default async function init(): Promise<void> {
+  await appInstall(NAME, DEFAULT_USER, {
+    [AUTHORIZATION_FORM]: {
+      [API_KEY]: 'api_key',
+      [API_SECRET]: 'api_secret',
+    },
+  });
+
+  const app = new VonageApplication();
+  container.setApplication(app);
+
+  const sendSMS = new VonageSendSMSConnector();
+
+  sendSMS
+    .setSender(sender)
+    .setDb(db)
+    .setApplication(app);
+  container.setConnector(sendSMS);
+}
