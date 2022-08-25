8d7b93e7366dd7e867e7c8879053c0ba906f3582
Tomas Novak <tomasnovak@DESKTOP-IC87HPL.localdomain>
2022-08-25 15:24:53 +0200

    Sendinblue: wip

--- lib/Sendinblue/Connector/SendinblueSendEmailConnector.ts
+++ lib/Sendinblue/Connector/SendinblueSendEmailConnector.ts
@@ -1,14 +1,28 @@
 import AConnector from '@orchesty/nodejs-sdk/dist/lib/Connector/AConnector';
 import ProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/ProcessDto';
+import HttpMethods from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
+import { IInput } from '../../Pipedrive/Connector/PipedriveAddLeadConnector';
+import { IOutput } from '../../Paypal/Connector/PaypalCreateOrderConnector';
 
 export const NAME = 'sendinblue-send-email-connector';
 
 export default class SendinblueSendEmailConnector extends AConnector {
-    public getName = (): string => NAME;
+  public getName = (): string => NAME;
 
-    public async processAction(_dto: ProcessDto): Promise<ProcessDto> {
-        const dto = _dto;
+  public async processAction(_dto: ProcessDto): Promise<ProcessDto> {
+    const dto = _dto;
+    const body = dto.jsonData as IInput;
 
-        return dto;
-    }
+    const appInstall = await this._getApplicationInstallFromProcess(dto);
+    const url = '/leads';
+    const req = await this._application.getRequestDto(dto, appInstall, HttpMethods.POST, url, body);
+    const resp = await this._sender.send(req, [201]);
+
+    dto.jsonData = resp.jsonBody as IOutput;
+    return dto;
+  }
+}
+
+export interface IInput {
+
 }
