ea2d6f85e561cc58bb85a933c6f9cc39abe2a083
Tomas Novak <tomasnovak@DESKTOP-IC87HPL.localdomain>
2022-08-26 13:15:29 +0200

    Productboard: test

--- lib/Productboard/Batch/__tests__/ProductboardListAllFeaturesBatch.ts
+++ lib/Productboard/Batch/__tests__/ProductboardListAllFeaturesBatch.ts
@@ -1,5 +1,5 @@
 import NodeTester from '@orchesty/nodejs-sdk/dist/test/Testers/NodeTester';
-import { productboardApp } from '../../../../test/DataProvider';
+import init from '../../../../test/Implementation/productboard';
 import { container } from '../../../../test/TestAbstract';
 import { NAME as PRODUCTBOARD_LIST_ALL_FEATURES_BATCH } from '../ProductboardListAllFeaturesBatch';
 
@@ -8,7 +8,7 @@ let tester: NodeTester;
 describe('Tests for ProductboardListAllFeaturesBatch', () => {
     beforeEach(async () => {
         tester = new NodeTester(container, __filename);
-        await productboardApp();
+        await init();
     });
 
     it('process - ok', async () => {
--- test/Implementation/productboard.ts
+++ test/Implementation/productboard.ts
@@ -1,39 +1,39 @@
 import { AUTHORIZATION_FORM } from '@orchesty/nodejs-sdk/dist/lib/Application/Base/AApplication';
 import { TOKEN } from '@orchesty/nodejs-sdk/dist/lib/Authorization/Type/Basic/ABasicApplication';
-import { appInstall, DEFAULT_ACCESS_TOKEN, DEFAULT_USER } from '../DataProvider';
-import { container, db, sender } from '../TestAbstract';
-import ProductboardApplication, { NAME as PRODUCTBOARD_APP } from '../../lib/Productboard/ProductboardApplication';
 import ProductboardListAllFeaturesBatch from '../../lib/Productboard/Batch/ProductboardListAllFeaturesBatch';
 import ProductboardListAllProductsBatch from '../../lib/Productboard/Batch/ProductboardListAllProductsBatch';
 import ProductboardCreateNewFeatureConnector
-  from '../../lib/Productboard/Connector/ProductboardCreateNewFeatureConnector';
+    from '../../lib/Productboard/Connector/ProductboardCreateNewFeatureConnector';
+import ProductboardApplication, { NAME as PRODUCTBOARD_APP } from '../../lib/Productboard/ProductboardApplication';
+import { appInstall, DEFAULT_ACCESS_TOKEN, DEFAULT_USER } from '../DataProvider';
+import { container, db, sender } from '../TestAbstract';
 
 export default async function init(): Promise<void> {
-  await appInstall(PRODUCTBOARD_APP, DEFAULT_USER, {
-    [AUTHORIZATION_FORM]: {
-      [TOKEN]: DEFAULT_ACCESS_TOKEN,
-    },
-  });
-  const app = new ProductboardApplication();
-  container.setApplication(app);
+    await appInstall(PRODUCTBOARD_APP, DEFAULT_USER, {
+        [AUTHORIZATION_FORM]: {
+            [TOKEN]: DEFAULT_ACCESS_TOKEN,
+        },
+    });
+    const app = new ProductboardApplication();
+    container.setApplication(app);
 
-  const listAllFeatures = new ProductboardListAllFeaturesBatch();
-  const listAllProducts = new ProductboardListAllProductsBatch();
-  const createNewFeature = new ProductboardCreateNewFeatureConnector();
+    const listAllFeatures = new ProductboardListAllFeaturesBatch();
+    const listAllProducts = new ProductboardListAllProductsBatch();
+    const createNewFeature = new ProductboardCreateNewFeatureConnector();
 
-  listAllFeatures
-    .setSender(sender)
-    .setDb(db)
-    .setApplication(app);
-  container.setBatch(listAllFeatures);
-  listAllProducts
-    .setSender(sender)
-    .setDb(db)
-    .setApplication(app);
-  container.setBatch(listAllProducts);
-  createNewFeature
-    .setSender(sender)
-    .setDb(db)
-    .setApplication(app);
-  container.setConnector(createNewFeature);
+    listAllFeatures
+        .setSender(sender)
+        .setDb(db)
+        .setApplication(app);
+    container.setBatch(listAllFeatures);
+    listAllProducts
+        .setSender(sender)
+        .setDb(db)
+        .setApplication(app);
+    container.setBatch(listAllProducts);
+    createNewFeature
+        .setSender(sender)
+        .setDb(db)
+        .setApplication(app);
+    container.setConnector(createNewFeature);
 }
