0d69ee69f231cc02690d88485dea6cbeaacb00e9
Martin Pilný <martin.pilny@hanaboso.com>
2022-08-26 10:52:58 +0200

    fixed

--- lib/OneDrive/Connector/OneDriveUploadFileConnector.ts
+++ lib/OneDrive/Connector/OneDriveUploadFileConnector.ts
@@ -33,8 +33,54 @@ export interface IInput {
 }
 
 export interface IOutput {
+    cTag: string,
+    createdBy: {
+        application: {
+            displayName: string,
+            id: string
+        },
+        user: {
+            displayName: string,
+            id: string
+        }
+    },
+    createdDateTime: string,
+    eTag: string,
+    file: {
+        hashes: {
+            quickXorHash: string,
+            sha1Hash: string,
+            sha256Hash: string
+        },
+        mimeType: string
+    },
+    fileSystemInfo: {
+        createdDateTime: string,
+        lastModifiedDateTime: string
+    },
     id: string,
+    lastModifiedBy: {
+        application: {
+            displayName: string,
+            id: string
+        },
+        user: {
+            displayName: string,
+            id: string
+        }
+    },
+    lastModifiedDateTime: string,
     name: string,
+    parentReference: {
+        driveId: string,
+        driveType: string,
+        id: string,
+        name: string,
+        path: string
+    },
+    reactions: {
+        commentCount: number
+    },
     size: number,
-    file: [];
+    webUrl: string
 }
