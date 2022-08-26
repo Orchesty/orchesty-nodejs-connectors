309a2368905bb67e53343796be2d5a874f366f22
Martin Pilný <martin.pilny@hanaboso.com>
2022-08-26 11:34:49 +0200

    fixed

--- lib/OneDrive/Connector/OneDriveUploadFileConnector.ts
+++ lib/OneDrive/Connector/OneDriveUploadFileConnector.ts
@@ -1,85 +1,88 @@
 import AConnector from '@orchesty/nodejs-sdk/dist/lib/Connector/AConnector';
+import { HttpMethods } from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
 import ProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/ProcessDto';
-import HttpMethods from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
+import OneDriveApplication from '../OneDriveApplication';
 
 export const NAME = 'one-drive-upload-file-connector';
 
 export default class OneDriveUploadFileConnector extends AConnector {
-  public getName = (): string => NAME;
 
-  public async processAction(_dto: ProcessDto): Promise<ProcessDto> {
-    const dto = _dto;
-    const { name, content } = dto.jsonData as IInput;
+    public getName(): string {
+        return NAME;
+    }
 
-    const appInstall = await this._getApplicationInstallFromProcess(dto);
-    const req = await this._application.getRequestDto(
-      dto,
-      appInstall,
-      HttpMethods.PUT,
-      `me/drive/root:/${name}:/content`,
-      content,
-    );
-    const resp = await this._sender.send(req, [201]);
-    dto.jsonData = resp.jsonBody as IOutput;
+    public async processAction(dto: ProcessDto<IInput>): Promise<ProcessDto<IOutput>> {
+        const { name, content } = dto.getJsonData();
 
-    return dto;
-  }
+        const appInstall = await this.getApplicationInstallFromProcess(dto);
+        const req = this.getApplication<OneDriveApplication>().getRequestDto(
+            dto,
+            appInstall,
+            HttpMethods.PUT,
+            `me/drive/root:/${name}:/content`,
+            content,
+        );
+        const resp = await this.getSender().send<IOutput>(req, [201]);
+
+        return dto.setNewJsonData(resp.getJsonBody());
+    }
+
 }
 export interface IInput {
-    content: string
-    name: string
+    content: string;
+    name: string;
 
 }
 
 export interface IOutput {
-    cTag: string,
+    cTag: string;
     createdBy: {
         application: {
-            displayName: string,
-            id: string
-        },
+            displayName: string;
+            id: string;
+        };
         user: {
-            displayName: string,
-            id: string
-        }
-    },
-    createdDateTime: string,
-    eTag: string,
+            displayName: string;
+            id: string;
+        };
+    };
+    createdDateTime: string;
+    eTag: string;
     file: {
         hashes: {
-            quickXorHash: string,
-            sha1Hash: string,
-            sha256Hash: string
-        },
-        mimeType: string
-    },
+            quickXorHash: string;
+            sha1Hash: string;
+            sha256Hash: string;
+        };
+        mimeType: string;
+    };
     fileSystemInfo: {
-        createdDateTime: string,
-        lastModifiedDateTime: string
-    },
-    id: string,
+        createdDateTime: string;
+        lastModifiedDateTime: string;
+    };
+    id: string;
     lastModifiedBy: {
         application: {
-            displayName: string,
-            id: string
-        },
+            displayName: string;
+            id: string;
+        };
         user: {
-            displayName: string,
-            id: string
-        }
-    },
-    lastModifiedDateTime: string,
-    name: string,
+            displayName: string;
+            id: string;
+        };
+    };
+    lastModifiedDateTime: string;
+    name: string;
     parentReference: {
-        driveId: string,
-        driveType: string,
-        id: string,
-        name: string,
-        path: string
-    },
+        driveId: string;
+        driveType: string;
+        id: string;
+        name: string;
+        path: string;
+    };
     reactions: {
-        commentCount: number
-    },
-    size: number,
-    webUrl: string
+        commentCount: number;
+    };
+    size: number;
+    webUrl: string;
 }
--- lib/OneDrive/OneDriveApplication.ts
+++ lib/OneDrive/OneDriveApplication.ts
@@ -1,62 +1,76 @@
-import AProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/AProcessDto';
-import Form from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/Form';
-import FormStack from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/FormStack';
-import HttpMethods from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
-import RequestDto from '@orchesty/nodejs-sdk/dist/lib/Transport/Curl/RequestDto';
-import { ApplicationInstall } from '@orchesty/nodejs-sdk/dist/lib/Application/Database/ApplicationInstall';
 import { AUTHORIZATION_FORM } from '@orchesty/nodejs-sdk/dist/lib/Application/Base/AApplication';
-import { CommonHeaders, JSON_TYPE } from '@orchesty/nodejs-sdk/dist/lib/Utils/Headers';
-import AOAuth2Application from '@orchesty/nodejs-sdk/dist/lib/Authorization/Type/OAuth2/AOAuth2Application';
+import { ApplicationInstall } from '@orchesty/nodejs-sdk/dist/lib/Application/Database/ApplicationInstall';
 import Field from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/Field';
 import FieldType from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/FieldType';
+import Form from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/Form';
+import FormStack from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/FormStack';
+import AOAuth2Application from '@orchesty/nodejs-sdk/dist/lib/Authorization/Type/OAuth2/AOAuth2Application';
 import { CLIENT_ID, CLIENT_SECRET } from '@orchesty/nodejs-sdk/dist/lib/Authorization/Type/OAuth2/IOAuth2Application';
+import RequestDto from '@orchesty/nodejs-sdk/dist/lib/Transport/Curl/RequestDto';
+import { HttpMethods } from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
+import AProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/AProcessDto';
+import { CommonHeaders, JSON_TYPE } from '@orchesty/nodejs-sdk/dist/lib/Utils/Headers';
 
 export const NAME = 'one-drive';
 export const TOKEN = 'token';
 
 export default class OneDriveApplication extends AOAuth2Application {
-  public getName = (): string => NAME;
 
-  public getPublicName = (): string => 'One Drive';
-
-  public getDescription = (): string => 'One Drive description';
-
-  public getFormStack = (): FormStack => {
-    const form = new Form(AUTHORIZATION_FORM, 'Authorization settings')
-      .addField(new Field(FieldType.TEXT, CLIENT_ID, 'Client id', null, true))
-      .addField(new Field(FieldType.TEXT, CLIENT_SECRET, 'Client secret', null, true));
-
-    return new FormStack().addForm(form);
-  };
-
-  public getRequestDto = (
-    dto: AProcessDto,
-    applicationInstall: ApplicationInstall,
-    method: HttpMethods,
-    _url?: string,
-    data?: unknown,
-  ): RequestDto => {
-    const settings = applicationInstall.getSettings();
-    const token = settings[AUTHORIZATION_FORM][TOKEN];
-    const url = `https://graph.microsoft.com/v1.0/${_url}`;
-    const request = new RequestDto(url, method, dto);
-    request.headers = {
-      [CommonHeaders.CONTENT_TYPE]: JSON_TYPE,
-      [CommonHeaders.ACCEPT]: JSON_TYPE,
-      [CommonHeaders.AUTHORIZATION]: `Bearer ${token}`,
-    };
-
-    if (data) {
-      request.setJsonBody(data);
+    public getName(): string {
+        return NAME;
     }
 
-    return request;
-  };
+    public getPublicName(): string {
+        return 'One Drive';
+    }
 
-  public getAuthUrl = (): string => 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize';
+    public getDescription(): string {
+        return 'One Drive description';
+    }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public getScopes = (applicationInstall: ApplicationInstall): string[] => ['Files.ReadWrite'];
+    public getFormStack(): FormStack {
+        const form = new Form(AUTHORIZATION_FORM, 'Authorization settings')
+            .addField(new Field(FieldType.TEXT, CLIENT_ID, 'Client id', null, true))
+            .addField(new Field(FieldType.TEXT, CLIENT_SECRET, 'Client secret', null, true));
 
-  public getTokenUrl = (): string => 'https://login.microsoftonline.com/common/oauth2/v2.0/token';
+        return new FormStack().addForm(form);
+    }
+
+    public getRequestDto(
+        dto: AProcessDto,
+        applicationInstall: ApplicationInstall,
+        method: HttpMethods,
+        _url?: string,
+        data?: unknown,
+    ): RequestDto {
+        const settings = applicationInstall.getSettings();
+        const token = settings[AUTHORIZATION_FORM][TOKEN];
+        const url = `https://graph.microsoft.com/v1.0/${_url}`;
+        const request = new RequestDto(url, method, dto);
+        request.setHeaders({
+            [CommonHeaders.CONTENT_TYPE]: JSON_TYPE,
+            [CommonHeaders.ACCEPT]: JSON_TYPE,
+            [CommonHeaders.AUTHORIZATION]: `Bearer ${token}`,
+        });
+
+        if (data) {
+            request.setJsonBody(data);
+        }
+
+        return request;
+    }
+
+    public getAuthUrl(): string {
+        return 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize';
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    public getScopes(applicationInstall: ApplicationInstall): string[] {
+        return ['Files.ReadWrite'];
+    }
+
+    public getTokenUrl(): string {
+        return 'https://login.microsoftonline.com/common/oauth2/v2.0/token';
+    }
+
 }
