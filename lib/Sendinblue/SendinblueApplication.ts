68855622b6c89cf7b1fbc9c322568df2fc92abbf
Tomas Novak <tomasnovak@DESKTOP-IC87HPL.localdomain>
2022-08-26 13:57:59 +0200

    Sendinblue: app + sendEmail connector

--- lib/Sendinblue/SendinblueApplication.ts
+++ lib/Sendinblue/SendinblueApplication.ts
@@ -1,51 +1,60 @@
-import AProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/AProcessDto';
-import Form from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/Form';
-import FormStack from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/FormStack';
-import HttpMethods from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
-import RequestDto from '@orchesty/nodejs-sdk/dist/lib/Transport/Curl/RequestDto';
-import { ABasicApplication } from '@orchesty/nodejs-sdk/dist/lib/Authorization/Type/Basic/ABasicApplication';
-import { ApplicationInstall } from '@orchesty/nodejs-sdk/dist/lib/Application/Database/ApplicationInstall';
 import { AUTHORIZATION_FORM } from '@orchesty/nodejs-sdk/dist/lib/Application/Base/AApplication';
-import { CommonHeaders, JSON_TYPE } from '@orchesty/nodejs-sdk/dist/lib/Utils/Headers';
+import { ApplicationInstall } from '@orchesty/nodejs-sdk/dist/lib/Application/Database/ApplicationInstall';
 import Field from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/Field';
 import FieldType from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/FieldType';
+import Form from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/Form';
+import FormStack from '@orchesty/nodejs-sdk/dist/lib/Application/Model/Form/FormStack';
+import { ABasicApplication } from '@orchesty/nodejs-sdk/dist/lib/Authorization/Type/Basic/ABasicApplication';
+import RequestDto from '@orchesty/nodejs-sdk/dist/lib/Transport/Curl/RequestDto';
+import { HttpMethods } from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
+import AProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/AProcessDto';
+import { CommonHeaders, JSON_TYPE } from '@orchesty/nodejs-sdk/dist/lib/Utils/Headers';
 
 export const NAME = 'sendinblue';
 export const API_KEY = 'api_key';
 
 export default class SendinblueApplication extends ABasicApplication {
-  public getName = (): string => NAME;
 
-  public getPublicName = (): string => 'Sendinblue';
-
-  public getDescription = (): string => 'Sendinblue description';
-
-  public getRequestDto = (
-    dto: AProcessDto,
-    applicationInstall: ApplicationInstall,
-    method: HttpMethods,
-    _url?: string,
-    data?: unknown,
-  ): RequestDto => {
-    const url = `https://api.sendinblue.com/v3/${_url}`;
-    const request = new RequestDto(url ?? '', method, dto);
-    request.headers = {
-      [CommonHeaders.CONTENT_TYPE]: JSON_TYPE,
-      [CommonHeaders.ACCEPT]: JSON_TYPE,
-      'api-key': applicationInstall.getSettings()[AUTHORIZATION_FORM][API_KEY],
-    };
-
-    if (data) {
-      request.setJsonBody(data);
+    public getName(): string {
+        return NAME;
     }
 
-    return request;
-  };
+    public getPublicName(): string {
+        return 'Sendinblue';
+    }
 
-  public getFormStack = (): FormStack => {
-    const form = new Form(AUTHORIZATION_FORM, 'Authorization settings')
-      .addField(new Field(FieldType.TEXT, API_KEY, 'API key', undefined, true));
+    public getDescription(): string {
+        return 'Sendinblue description';
+    }
 
-    return new FormStack().addForm(form);
-  };
+    public getRequestDto(
+        dto: AProcessDto,
+        applicationInstall: ApplicationInstall,
+        method: HttpMethods,
+        _url?: string,
+        data?: unknown,
+    ): RequestDto {
+        const url = `https://api.sendinblue.com/v3/${_url}`;
+        const request = new RequestDto(url ?? '', method, dto);
+        request.setHeaders({
+            [CommonHeaders.CONTENT_TYPE]: JSON_TYPE,
+            [CommonHeaders.ACCEPT]: JSON_TYPE,
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            'api-key': applicationInstall.getSettings()[AUTHORIZATION_FORM][API_KEY],
+        });
+
+        if (data) {
+            request.setJsonBody(data);
+        }
+
+        return request;
+    }
+
+    public getFormStack(): FormStack {
+        const form = new Form(AUTHORIZATION_FORM, 'Authorization settings')
+            .addField(new Field(FieldType.TEXT, API_KEY, 'API key', undefined, true));
+
+        return new FormStack().addForm(form);
+    }
+
 }
