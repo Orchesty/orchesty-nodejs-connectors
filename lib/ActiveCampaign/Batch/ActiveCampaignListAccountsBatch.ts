bfd0e58627b602eedd4e0cc4dec7a871cfe18bd3
Martin Pilný <martin.pilny@hanaboso.com>
2022-08-26 13:08:41 +0200

    ActiveCampaign: created listaccounts batch

---
+++ lib/ActiveCampaign/Batch/ActiveCampaignListAccountsBatch.ts
@@ -0,0 +1,49 @@
+import ABatchNode from '@orchesty/nodejs-sdk/dist/lib/Batch/ABatchNode';
+import { HttpMethods } from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
+import BatchProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/BatchProcessDto';
+
+export const NAME = 'active-campaign-list-accounts-batch';
+const LIMIT = 50;
+
+export default class ActiveCampaignListAccountsBatch extends ABatchNode {
+
+    public getName(): string {
+        return NAME;
+    }
+
+    public async processAction(dto: BatchProcessDto): Promise<BatchProcessDto> {
+        const offset = Number(dto.getBatchCursor('1'));
+        const appInstall = await this.getApplicationInstallFromProcess(dto);
+        const req = await this.getApplication().getRequestDto(
+            dto,
+            appInstall,
+            HttpMethods.GET,
+            `accounts?offset=${offset}&limit=${LIMIT}`,
+        );
+        const resp = await this.getSender().send<IResponse>(req, [200]);
+        const response = resp.getJsonBody();
+        dto.setItemList(response.accounts ?? []);
+        if (response.total_records >= LIMIT) {
+            dto.setBatchCursor((offset + LIMIT).toString());
+        }
+        return dto;
+    }
+
+}
+
+/* eslint-disable @typescript-eslint/naming-convention */
+interface IResponse {
+    accounts: IOutput[];
+    total_records: number;
+}
+
+export interface IOutput {
+    name: string;
+    createdTimestamp: string;
+    updatedTimestamp: string;
+    contactCount: string;
+    dealCount: string;
+    links: string[];
+    id: string;
+}
+/* eslint-enable @typescript-eslint/naming-convention */
