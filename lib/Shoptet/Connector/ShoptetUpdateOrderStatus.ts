07ea8ab13d69055703b0f24844534d94aaeb6019
Radek Jirsa <jirsa.r@ghanaboso.com>
2022-08-29 11:06:13 +0200

    Deps updated

---
+++ lib/Shoptet/Connector/ShoptetUpdateOrderStatus.ts
@@ -0,0 +1,42 @@
+import AConnector from '@orchesty/nodejs-sdk/dist/lib/Connector/AConnector';
+import { HttpMethods } from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
+import ProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/ProcessDto';
+import { BASE_URL } from '../ABaseShoptet';
+import APluginShoptetApplication from '../APluginShoptetApplication';
+
+export const NAME = 'shoptet-update-order-status';
+
+export default class ShoptetUpdateOrderStatus extends AConnector {
+
+    public getName(): string {
+        return NAME;
+    }
+
+    public async processAction(dto: ProcessDto<IInput>): Promise<ProcessDto> {
+        const app = this.getApplication<APluginShoptetApplication>();
+
+        const { id, status } = dto.getJsonData();
+        const appInstall = await this.getApplicationInstallFromProcess(dto);
+        const url = `${BASE_URL}/api/orders/${id}/status`;
+        const requestDto = await app.getRequestDto(
+            dto,
+            appInstall,
+            HttpMethods.PATCH,
+            url,
+            JSON.stringify(status),
+        );
+        await this.getSender().send(requestDto, [200, 404]);
+
+        return dto;
+    }
+
+}
+
+export interface IInput {
+    id: string;
+    status: {
+        data: {
+            statusId: number;
+        };
+    };
+}
---
+++ lib/Shoptet/Connector/ShoptetUpdateRemarkForOrder.ts
@@ -0,0 +1,43 @@
+import AConnector from '@orchesty/nodejs-sdk/dist/lib/Connector/AConnector';
+import { HttpMethods } from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
+import ProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/ProcessDto';
+import { BASE_URL } from '../ABaseShoptet';
+import APluginShoptetApplication from '../APluginShoptetApplication';
+
+export const NAME = 'shoptet-update-remark-for-order';
+
+export default class ShoptetUpdateRemarkForOrder extends AConnector {
+
+    public getName(): string {
+        return NAME;
+    }
+
+    public async processAction(dto: ProcessDto<IInput>): Promise<ProcessDto> {
+        const { id, notes } = dto.getJsonData();
+
+        const app = this.getApplication<APluginShoptetApplication>();
+        const appInstall = await this.getApplicationInstallFromProcess(dto);
+
+        const url = `${BASE_URL}/api/orders/${id}/notes`;
+        const requestDto = await app.getRequestDto(
+            dto,
+            appInstall,
+            HttpMethods.PATCH,
+            url,
+            JSON.stringify(notes),
+        );
+        await this.getSender().send(requestDto, [200, 404]);
+
+        return dto;
+    }
+
+}
+
+export interface IInput {
+    id: string;
+    notes: {
+        data: {
+            trackingNumber: string;
+        };
+    };
+}
