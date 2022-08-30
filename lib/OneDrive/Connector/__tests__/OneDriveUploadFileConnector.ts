3386f72b40339bd33d769d63585c40addca179a1
Martin Pilny <martin.pilny2000@gmail.com>
2022-08-30 08:38:09 +0000

    OneDrive: created upload file connector

---
+++ lib/OneDrive/Connector/__tests__/OneDriveUploadFileConnector.ts
@@ -0,0 +1,17 @@
+import NodeTester from '@orchesty/nodejs-sdk/dist/test/Testers/NodeTester';
+import init from '../../../../test/Implementation/oneDrive';
+import { container } from '../../../../test/TestAbstract';
+import { NAME as ONE_DRIVE_UPLOAD_FILE_CONNECTOR } from '../OneDriveUploadFileConnector';
+
+let tester: NodeTester;
+
+describe('Tests for OneDriveUploadFileConnector', () => {
+    beforeEach(async () => {
+        tester = new NodeTester(container, __filename);
+        await init();
+    });
+
+    it('process - ok', async () => {
+        await tester.testConnector(ONE_DRIVE_UPLOAD_FILE_CONNECTOR);
+    });
+});
