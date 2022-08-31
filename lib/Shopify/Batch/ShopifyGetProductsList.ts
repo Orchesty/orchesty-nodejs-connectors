00610f14f53797b4c3cc75c11a151289bb982c66
Jakub Sobotka <sobotka.j@hanaboso.com>
2022-08-31 13:47:44 +0200

    shopify get products list

---
+++ lib/Shopify/Batch/ShopifyGetProductsList.ts
@@ -0,0 +1,75 @@
+import { ApplicationInstall } from '@orchesty/nodejs-sdk/dist/lib/Application/Database/ApplicationInstall';
+import ABatchNode from '@orchesty/nodejs-sdk/dist/lib/Batch/ABatchNode';
+import { HttpMethods } from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
+import BatchProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/BatchProcessDto';
+import ShopifyApplication from '../ShopifyApplication';
+
+export const NAME = 'shopify-get-products-list';
+
+const LIMIT = 100;
+const LIST_PRODUCTS_ENDPOINT = `admin/api/2022-07/products.json?limit=${LIMIT}`;
+
+const LAST_RUN_KEY = 'lastRunListProductsChanges';
+
+export default class ShopifyGetProductsList extends ABatchNode {
+
+    public getName(): string {
+        return NAME;
+    }
+
+    public async processAction(dto: BatchProcessDto<IInputJson>): Promise<BatchProcessDto> {
+        const { from } = dto.getJsonData();
+
+        const appInstall = await this.getApplicationInstallFromProcess(dto);
+
+        const creationTimeFrom = from || appInstall.getNonEncryptedSettings()[LAST_RUN_KEY];
+
+        let url = dto.getBatchCursor(LIST_PRODUCTS_ENDPOINT);
+
+        if (creationTimeFrom) {
+            const separatorChar = url.includes('?') ? '&' : '?';
+            url = `${url}${separatorChar}created_at_min=${creationTimeFrom}`;
+        }
+
+        const app = this.getApplication<ShopifyApplication>();
+        const requestDto = app.getRequestDto(dto, appInstall, HttpMethods.GET, url);
+
+        const res = await this.getSender().send<IResponseJson>(requestDto);
+        const linkHeaders = res.getHeaders().get('Link');
+
+        if (linkHeaders) {
+            const nextLinkHeader = linkHeaders.split(',').find((link) => link.includes('next'));
+            // eslint-disable-next-line prefer-named-capture-group
+            const nextLink = (/<(.+)>/).exec(nextLinkHeader ?? '');
+            if (nextLink) {
+                dto.setBatchCursor(nextLink[1]);
+            } else {
+                await this.writeLastTimeRun(appInstall);
+            }
+        } else {
+            await this.writeLastTimeRun(appInstall);
+        }
+
+        const { products } = res.getJsonBody();
+
+        dto.setItemList(products);
+
+        return dto;
+    }
+
+    private async writeLastTimeRun(appInstall: ApplicationInstall): Promise<void> {
+        appInstall.addNonEncryptedSettings({ [LAST_RUN_KEY]: new Date() });
+        await (await this.getDbClient().getApplicationRepository()).update(appInstall);
+    }
+
+}
+
+interface IInputJson {
+    from: string;
+}
+
+interface IResponseJson {
+    products: {
+        id: string;
+    }[];
+}
