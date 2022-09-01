58ee9d713e6db97169009c7aeadd1f1e46bc30de
Jakub Sobotka <sobotka.j@hanaboso.com>
2022-09-01 10:35:18 +0200

    change interface

--- lib/Authentica/Connector/AuthenticaPutOrders.ts
+++ lib/Authentica/Connector/AuthenticaPutOrders.ts
@@ -29,6 +29,7 @@ export default class AuthenticaPutOrders extends AConnector {
 }
 
 export interface IOrder {
+    id: string;
     orderNumber: string;
     destination: {
         firstName: string;
