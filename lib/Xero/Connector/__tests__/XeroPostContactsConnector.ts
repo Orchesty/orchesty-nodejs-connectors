a9511bfcca2026259637817ac2f88a6816f09a86
Fila <fila.r@hanaboso.com>
2022-09-30 15:07:03 +0200

    Fix lint errors

--- lib/Xero/Connector/__tests__/XeroPostContactsConnector.ts
+++ lib/Xero/Connector/__tests__/XeroPostContactsConnector.ts
@@ -1,7 +1,7 @@
 import NodeTester from '@orchesty/nodejs-sdk/dist/test/Testers/NodeTester';
 import init from '../../../../test/Implementation/xero';
 import { container } from '../../../../test/TestAbstract';
-import { NAME as XERO_PUT_CONTACTS_CONNECTOR } from '../XeroPutContactsConnector';
+import { NAME as XERO_PUT_CONTACTS_CONNECTOR } from '../XeroPostContactsConnector';
 
 let tester: NodeTester;
 
