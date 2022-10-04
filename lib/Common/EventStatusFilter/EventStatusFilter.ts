2d4dda8adb0dfaf05c8032107e47456083f4e61b
Fila <fila.r@hanaboso.com>
2022-10-04 12:56:53 +0200

    Event status filter

---
+++ lib/Common/EventStatusFilter/EventStatusFilter.ts
@@ -0,0 +1,38 @@
+import ACommonNode from '@orchesty/nodejs-sdk/dist/lib/Commons/ACommonNode';
+import ProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/ProcessDto';
+import ResultCode from '@orchesty/nodejs-sdk/dist/lib/Utils/ResultCode';
+
+export const NAME = 'event-status-filter';
+
+export default class EventStatusFilter extends ACommonNode {
+
+    public constructor(private readonly type: string) {
+        super();
+    }
+
+    public getName(): string {
+        return NAME;
+    }
+
+    public processAction(dto: ProcessDto<IInput>): ProcessDto<IInput> {
+        const data = dto.getJsonData();
+        if (data.type !== this.type) {
+            dto.setStopProcess(ResultCode.DO_NOT_CONTINUE, 'Filtered out!');
+        }
+
+        return dto;
+    }
+
+}
+
+export interface IInput {
+    type: string;
+    data: {
+        topologyId: string;
+        correlationId: string;
+        processId: string;
+        user: string;
+        timestampMs: number;
+        resultMessage: string;
+    };
+}
