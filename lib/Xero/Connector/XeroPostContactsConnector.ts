a2b1f5db3eaf7dca28444651c9ab15095a13a462
Fila <fila.r@hanaboso.com>
2022-11-16 15:58:35 +0100

    wip

--- lib/Xero/Connector/XeroPostContactsConnector.ts
+++ lib/Xero/Connector/XeroPostContactsConnector.ts
@@ -1,4 +1,5 @@
 import AConnector from '@orchesty/nodejs-sdk/dist/lib/Connector/AConnector';
+import ResponseDto from '@orchesty/nodejs-sdk/dist/lib/Transport/Curl/ResponseDto';
 import { HttpMethods } from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
 import ProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/ProcessDto';
 
@@ -10,7 +11,7 @@ export default class XeroPostContactsConnector extends AConnector {
         return NAME;
     }
 
-    public async processAction(dto: ProcessDto<IInput>): Promise<ProcessDto> {
+    public async processAction(dto: ProcessDto<IInput>): Promise<ProcessDto<IOutput>> {
         const req = await this.getApplication().getRequestDto(
             dto,
             await this.getApplicationInstallFromProcess(dto),
@@ -18,9 +19,17 @@ export default class XeroPostContactsConnector extends AConnector {
             'contacts',
             JSON.stringify(dto.getJsonData()),
         );
-        const resp = await this.getSender().send(req, [200]);
+        const resp = await this.getSender().send<IResponse>(req, this.getCodeRange());
 
-        return dto.setNewJsonData(resp.getJsonBody());
+        return this.setNewJsonData(dto, resp);
+    }
+
+    protected setNewJsonData(dto: ProcessDto, resp: ResponseDto<IResponse>): ProcessDto<IOutput> {
+        return dto.setNewJsonData({ contact: resp.getJsonBody() });
+    }
+
+    protected getCodeRange(): number[] {
+        return [200];
     }
 
 }
@@ -60,6 +69,45 @@ export interface IInput {
     TrackingCategoryName?: string;
     TrackingOptionName?: string;
     PaymentTerms?: unknown;
+}
+
+export interface IOutput {
+    contact: IResponse | null;
+}
+
+export interface IResponse {
+    Contacts: {
+        Addresses: {
+            AddressType: string;
+            City: string;
+            Country: string;
+            PostalCode: string;
+            Region: string;
+        }[];
+        BankAccountDetails: string;
+        ContactGroups: unknown[];
+        ContactID: string;
+        ContactPersons: unknown[];
+        ContactStatus: string;
+        EmailAddress: string;
+        HasValidationErrors: boolean;
+        IsCustomer: boolean;
+        IsSupplier: boolean;
+        Name: string;
+        Phones: {
+            PhoneAreaCode: string;
+            PhoneCountryCode: string;
+            PhoneNumber: string;
+            PhoneType: string;
+        }[];
+        PurchasesTrackingCategories: unknown[];
+        SalesTrackingCategories: unknown[];
+        UpdatedDateUTC: string;
+    }[];
+    DateTimeUTC: string;
+    Id: string;
+    ProviderName: string;
+    Status: string;
 }
 
 /* eslint-enable @typescript-eslint/naming-convention */
