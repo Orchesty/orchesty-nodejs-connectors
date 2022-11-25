296b60b67b10d9ae5d6c351d744deca9a6ec8074
Fila <fila.r@hanaboso.com>
2022-11-25 10:43:55 +0100

    Tax rates batch

---
+++ lib/QuickBooks/Batch/__tests__/QuickBooksGetTaxRatesBatch.ts
@@ -0,0 +1,17 @@
+import NodeTester from '@orchesty/nodejs-sdk/dist/test/Testers/NodeTester';
+import init from '../../../../test/Implementation/quickBooks';
+import { container } from '../../../../test/TestAbstract';
+import { NAME as QUICK_BOOKS_GET_TAX_RATES_BATCH } from '../QuickBooksGetTaxRatesBatch';
+
+let tester: NodeTester;
+
+describe('Tests for QuickBooksGetTaxRatesBatch', () => {
+    beforeEach(async () => {
+        tester = new NodeTester(container, __filename);
+        await init();
+    });
+
+    it('process - ok', async () => {
+        await tester.testBatch(QUICK_BOOKS_GET_TAX_RATES_BATCH);
+    });
+});
