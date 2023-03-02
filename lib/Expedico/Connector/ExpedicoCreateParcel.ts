32c23208833d3f25d3c8e8be2c96d131042820c7
Fila <fila.r@hanaboso.com>
2023-03-02 16:04:43 +0100

    CreateParcel

---
+++ lib/Expedico/Connector/ExpedicoCreateParcel.ts
@@ -0,0 +1,73 @@
+import AConnector from '@orchesty/nodejs-sdk/dist/lib/Connector/AConnector';
+import { HttpMethods } from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
+import ProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/ProcessDto';
+
+export const NAME = 'expedico-create-parcel';
+
+export default class ExpedicoCreateParcel extends AConnector {
+
+    public getName(): string {
+        return NAME;
+    }
+
+    public async processAction(dto: ProcessDto<IInput>): Promise<ProcessDto<IOutput>> {
+        const data = dto.getJsonData();
+
+        const requestDto = await this.getApplication().getRequestDto(
+            dto,
+            await this.getApplicationInstallFromProcess(dto),
+            HttpMethods.POST,
+            // TODO rich kde se bude nastavovat base URL, bude to ve formulari?
+            'parcels',
+            JSON.stringify(data),
+        );
+
+        const { id, 'carrier-barcode': carrierBarcode, 'carrier-tracking-code': carrierTrackingCode } = (await this.getSender().send(requestDto, [201])).getHeaders();
+
+        return dto.setNewJsonData({
+            id,
+            carrierBarcode,
+            carrierTrackingCode,
+        } as IOutput);
+    }
+
+}
+
+export interface IAddress {
+    firstname: string;
+    lastname: string;
+    city: string;
+    zipCode: string;
+    street: string;
+    streetNumber: string;
+    email: string;
+    phone: string;
+    country: string;
+}
+
+export interface IInput {
+    data: {
+        carrier: string;
+        weightInKg: number;
+        sender: IAddress;
+        recipient: {
+            firstname: string;
+            lastname: string;
+            city: string;
+            zipCode: string;
+            street: string;
+            streetNumber: string;
+            email: string;
+            phone: string;
+            country: string;
+        };
+        carrierPickupDate: string;
+        orderNumber: string;
+    };
+}
+
+export interface IOutput {
+    id: number;
+    carrierBarcode: number;
+    carrierTrackingCode: number;
+}
