16bb224c5e6b04e5701a048fd6d998bbb672e65c
Radek Bruha <bruha.r@hanaboso.com>
2023-03-23 16:04:02 +0100

    Create ShopifyUpdateOrder connector

---
+++ lib/Shopify/Connector/__tests__/ShopifyUpdateOrder.ts
@@ -0,0 +1,17 @@
+import NodeTester from '@orchesty/nodejs-sdk/dist/test/Testers/NodeTester';
+import init from '../../../../test/Implementation/shopify';
+import { container } from '../../../../test/TestAbstract';
+import { NAME as SHOPIFY_UPDATE_ORDER } from '../ShopifyUpdateOrder';
+
+let tester: NodeTester;
+
+describe('Tests for ShopifyUpdateOrder', () => {
+    beforeAll(() => {
+        tester = new NodeTester(container, __filename);
+        init();
+    });
+
+    it('process - ok', async () => {
+        await tester.testConnector(SHOPIFY_UPDATE_ORDER);
+    });
+});
