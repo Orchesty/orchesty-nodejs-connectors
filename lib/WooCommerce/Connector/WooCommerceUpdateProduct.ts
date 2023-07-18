1afcb802d056f29757c8da60cc80fde5b4311463
Fila <fila.r@hanaboso.com>
2023-07-18 11:13:32 +0200

    Update code ranges

--- lib/WooCommerce/Connector/WooCommerceUpdateProduct.ts
+++ lib/WooCommerce/Connector/WooCommerceUpdateProduct.ts
@@ -1,4 +1,5 @@
 import AConnector from '@orchesty/nodejs-sdk/dist/lib/Connector/AConnector';
+import { defaultRanges, IResultRanges } from '@orchesty/nodejs-sdk/dist/lib/Transport/Curl/ResultCodeRange';
 import { HttpMethods } from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
 import ProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/ProcessDto';
 import { IOutput as IInput } from '../Batch/WooCommerceGetProducts';
@@ -21,9 +22,13 @@ export default class WooCommerceUpdateProduct extends AConnector {
             `wp-json/wc/v3/products/${id}`,
             JSON.stringify(rest),
         );
-        const resp = await this.getSender().send<IResponse>(req, [200]);
+        const resp = await this.getSender().send<IResponse>(req, this.getCodeRanges());
 
         return dto.setNewJsonData(resp.getJsonBody());
+    }
+
+    protected getCodeRanges(): IResultRanges {
+        return defaultRanges;
     }
 
 }
