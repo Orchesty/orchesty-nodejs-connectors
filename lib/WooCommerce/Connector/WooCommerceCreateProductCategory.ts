b3dd2423fb30940edda82b6a4f23a68124e6a7f2
Fila <fila.r@hanaboso.com>
2023-07-18 11:03:57 +0200

    Update connectors

--- lib/WooCommerce/Connector/WooCommerceCreateProductCategory.ts
+++ lib/WooCommerce/Connector/WooCommerceCreateProductCategory.ts
@@ -31,30 +31,30 @@ type IOutput = IInput;
 
 /* eslint-disable @typescript-eslint/naming-convention */
 export interface IInput {
-    id: number;
-    name: string;
-    slug: string;
-    parent: number;
-    description: string;
-    display: string;
-    image: {
-        id: number;
-        date_created: string;
-        date_created_gmt: string;
-        date_modified: string;
-        date_modified_gmt: string;
-        src: string;
-        name: string;
-        alt: string;
+    id?: number;
+    name?: string;
+    slug?: string;
+    parent?: number;
+    description?: string;
+    display?: string;
+    image?: {
+        id?: number;
+        date_created?: string;
+        date_created_gmt?: string;
+        date_modified?: string;
+        date_modified_gmt?: string;
+        src?: string;
+        name?: string;
+        alt?: string;
     };
-    menu_order: number;
-    count: number;
-    _links: {
-        self: {
-            href: string;
+    menu_order?: number;
+    count?: number;
+    _links?: {
+        self?: {
+            href?: string;
         }[];
-        collection: {
-            href: string;
+        collection?: {
+            href?: string;
         }[];
     };
 }
