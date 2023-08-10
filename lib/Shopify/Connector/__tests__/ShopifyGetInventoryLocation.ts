f7890aab0afa8f01d2dfa6efe5520ae2a924d8c3
Fila <fila.r@hanaboso.com>
2023-08-10 11:53:03 +0200

    Add ShopifyGetInventoryLocation

---
+++ lib/Shopify/Connector/__tests__/ShopifyGetInventoryLocation.ts
@@ -0,0 +1,17 @@
+import NodeTester from '@orchesty/nodejs-sdk/dist/test/Testers/NodeTester';
+import init from '../../../../test/Implementation/shopify';
+import { container } from '../../../../test/TestAbstract';
+import { NAME as SHOPIFY_GET_INVENTORY_LOCATION } from '../ShopifyGetInventoryLocation';
+
+let tester: NodeTester;
+
+describe('Tests for ShopifyGetInventoryLocation', () => {
+    beforeAll(() => {
+        tester = new NodeTester(container, __filename, true);
+        init();
+    });
+
+    it('process - ok', async () => {
+        await tester.testConnector(SHOPIFY_GET_INVENTORY_LOCATION);
+    });
+});
