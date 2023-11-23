36abfd72c8e8c22a7c3be5183ecdfe999e28be57
Richard Fíla <fila.r@hanaboso.com>
2023-11-23 09:23:30 +0000

    HEL-312

--- lib/Servant/Connector/ABaseServantSoapConnector.ts
+++ lib/Servant/Connector/ABaseServantSoapConnector.ts
@@ -3,23 +3,42 @@ import OnRepeatException from '@orchesty/nodejs-sdk/dist/lib/Exception/OnRepeatE
 import logger from '@orchesty/nodejs-sdk/dist/lib/Logger/Logger';
 import BatchProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/BatchProcessDto';
 import ProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/ProcessDto';
+import ResultCode from '@orchesty/nodejs-sdk/dist/lib/Utils/ResultCode';
 import * as soap from 'soap';
 import ServantApplication from '../ServantApplication';
 
-export function log<T>(dto: BatchProcessDto | ProcessDto, res: IResult & T, resultKey: string): T {
+export function log<T>(
+    dto: BatchProcessDto | ProcessDto,
+    res: IResult & T,
+    resultKey: string,
+): T {
     const data = (res as never)[resultKey] as T;
     const { result } = res;
 
+    let resultText: string | undefined = '';
+    let success: boolean | undefined = true;
+    if (resultKey === 'importRequestResult') {
+        // eslint-disable-next-line @typescript-eslint/prefer-destructuring
+        success = res?.importRequestResult?.success;
+        resultText = `${res?.importRequestResult?.result}: ${res?.importRequestResult?.infoText}`;
+    }
+
     if (result.resultCode === 1 || result.resultCode === 2) {
         logger.error(
-            `Request process failed. Message: [${dto.getHeader(result.resultString) ?? ''}]`,
+            `Request process failed. Message: [${resultText ?? dto.getHeader(result.resultString) ?? ''}]`,
             dto,
         );
+
+        dto.setStopProcess(ResultCode.STOP_AND_FAILED, result.resultString);
     } else {
         logger.info(
-            `Request successfully processed. Message: [${JSON.stringify(data) ?? ''}]`,
+            `Request successfully processed. Message: [${resultText ?? JSON.stringify(data) ?? ''}]`,
             dto,
         );
+
+        if (!success) {
+            dto.setStopProcess(ResultCode.STOP_AND_FAILED, resultText ?? JSON.stringify(data) ?? '');
+        }
     }
 
     return data;
@@ -75,5 +94,10 @@ export interface IResult {
     result: {
         resultCode: number;
         resultString: string;
+    };
+    importRequestResult?: {
+        success: boolean;
+        result: string;
+        infoText: string;
     };
 }
