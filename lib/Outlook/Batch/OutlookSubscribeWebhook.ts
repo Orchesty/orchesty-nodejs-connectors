4f7b3e6d36fd299d24fc3e9fc7fcf87aba26a66a
Richard Fíla <fila.r@hanaboso.com>
2024-02-01 12:19:07 +0000

    Outlook subscribe webhook

---
+++ lib/Outlook/Batch/OutlookSubscribeWebhook.ts
@@ -0,0 +1,92 @@
+import { ApplicationInstall } from '@orchesty/nodejs-sdk/dist/lib/Application/Database/ApplicationInstall';
+import Webhook from '@orchesty/nodejs-sdk/dist/lib/Application/Database/Webhook';
+import ABatchNode from '@orchesty/nodejs-sdk/dist/lib/Batch/ABatchNode';
+import { orchestyOptions } from '@orchesty/nodejs-sdk/dist/lib/Config/Config';
+import { HttpMethods } from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
+import BatchProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/BatchProcessDto';
+import ResultCode from '@orchesty/nodejs-sdk/dist/lib/Utils/ResultCode';
+import crypto from 'crypto';
+import OutlookApplication from '../OutlookApplication';
+
+export const NAME = 'outlook-subscribe-webhook';
+
+export default class OutlookSubscribeWebhook extends ABatchNode {
+
+    public getName(): string {
+        return NAME;
+    }
+
+    public async processAction(dto: BatchProcessDto): Promise<BatchProcessDto> {
+        const app = this.getApplication<OutlookApplication>();
+
+        const webhookIndex = Number(dto.getBatchCursor('0'));
+        const webhooks = app.getWebhookSubscriptions();
+
+        if (webhooks.length > 0) {
+            const appInstall = await this.getApplicationInstallFromProcess(dto);
+            const repo = this.getDbClient().getRepository(Webhook);
+
+            const webhook = webhooks[webhookIndex];
+            const backendUrl = orchestyOptions.backend;
+            const token = this.getRandomToken();
+            const requestDto = app.getRequestDto(
+                dto,
+                appInstall,
+                HttpMethods.POST,
+                '/subscriptions',
+                {
+                    notificationUrl: `${backendUrl}/api/applications/${app.getName()}/sync/notificationCallback?pf_user=${dto.getUser()}&pf_wh_token=${token}`,
+                    changeType: webhook.getParameters().changeType,
+                    resource: webhook.getName(),
+                    expirationDateTime: '3000-01-01T00:00:00.0000000Z',
+                },
+            );
+
+            let res;
+            try {
+                res = await this.getSender().send<IResponseJson>(requestDto, [201]);
+                const respBody = res.getJsonBody();
+
+                await repo.insert(new Webhook()
+                    .setWebhookId(respBody.id)
+                    .setUser(appInstall.getUser())
+                    .setNode(webhooks[webhookIndex].getNode())
+                    .setToken(token)
+                    .setApplication(app.getName())
+                    .setTopology(webhooks[webhookIndex].getTopology())
+                    .setName(webhooks[webhookIndex].getName()));
+
+                if (webhooks.length - 1 > webhookIndex) {
+                    dto.setBatchCursor((webhookIndex + 1).toString());
+                }
+            } catch (e) {
+                appInstall.setEnabled(false);
+                await this.getDbClient().getRepository(ApplicationInstall).update(appInstall);
+
+                dto.setStopProcess(ResultCode.STOP_AND_FAILED, res?.getBody() ?? 'Webhook could not be registered!');
+            }
+        }
+
+        return dto;
+    }
+
+    private getRandomToken(): string {
+        return crypto.randomBytes(64).toString('hex');
+    }
+
+}
+
+export interface IResponseJson {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    '@odata.context': string;
+    id: string;
+    resource: string;
+    applicationId: string;
+    changeType: string;
+    clientState: string;
+    notificationUrl: string;
+    expirationDateTime: string;
+    creatorId: string;
+    latestSupportedTlsVersion: string;
+    notificationContentType: string;
+}
---
+++ lib/Outlook/Batch/__tests__/OutlookSubscribeWebhook.ts
@@ -0,0 +1,17 @@
+import NodeTester from '@orchesty/nodejs-sdk/dist/test/Testers/NodeTester';
+import init from '../../../../test/Implementation/outlook';
+import { container } from '../../../../test/TestAbstract';
+import { NAME as OUTLOOK_SUBSCRIBE_WEBHOOK } from '../OutlookSubscribeWebhook';
+
+let tester: NodeTester;
+
+describe('Tests for OutlookSubscribeWebhook', () => {
+    beforeAll(() => {
+        tester = new NodeTester(container, __filename);
+        init();
+    });
+
+    it('process - ok', async () => {
+        await tester.testBatch(OUTLOOK_SUBSCRIBE_WEBHOOK);
+    });
+});
