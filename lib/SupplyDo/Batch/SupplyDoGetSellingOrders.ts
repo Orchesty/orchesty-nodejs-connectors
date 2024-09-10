da8b78dc10deb73d27ca9524404ccff3405f8cad
Radek Jirsa <jirsa.r@hanaboso.com>
2024-09-10 15:24:12 +0200

    SupplyDo: updated interfaces

--- lib/SupplyDo/Batch/SupplyDoGetSellingOrders.ts
+++ lib/SupplyDo/Batch/SupplyDoGetSellingOrders.ts
@@ -20,7 +20,7 @@ export default class SupplyDoGetSellingOrders extends ABatchNode {
         let url = 'items/selling_order?fields[]=*&fields[]=selling_order_history.*&fields[]=selling_order_product.*'
             + '&fields[]=selling_order_product.return_product.*&fields[]=selling_order_product.reclamation_product.*'
             + '&fields[]=customer.address.*&fields[]=selling_order_product.product_batch.*&fields[]=selling_order_product.product_batch.product.*'
-            + '&fields[]=total_price.*&fields[]=transport.*'
+            + '&fields[]=total_price.*&fields[]=transport.*&fields[]=customer.*'
             + '&filter[selling_order_history][_some][type][_eq]=processing'
             + '&filter[selling_order_history][_none][type][_in]=dispatched,delivered,not_delivered,not_paid,wrong_address,wrong_phone_number,package_lost'
             + `&filter[ecommerce][_eq]=${ecommerce}`
@@ -57,6 +57,8 @@ export default class SupplyDoGetSellingOrders extends ABatchNode {
 
 export interface ISellingOrder {
     customer: {
+        email: string;
+        phone: string;
         address: {
             city: string;
             country: string;
@@ -64,8 +66,6 @@ export interface ISellingOrder {
             street: string;
             street_number: string;
             zip_code: string;
-            email: string;
-            phone: string;
         };
     };
     id: string;
