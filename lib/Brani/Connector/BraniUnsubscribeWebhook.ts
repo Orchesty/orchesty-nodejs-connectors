a1fd3245ac2bd4cb7cb837667893fd2baa518e12
Radek Jirsa <jirsa.r@hanaboso.com>
2025-10-10 09:32:04 +0200

    Brani: UnsubscribeWebhook: added db clean

--- lib/Brani/Connector/BraniUnsubscribeWebhook.ts
+++ lib/Brani/Connector/BraniUnsubscribeWebhook.ts
@@ -1,3 +1,5 @@
+import Webhook from '@orchesty/nodejs-sdk/dist/lib/Application/Database/Webhook';
+import WebhookRepository from '@orchesty/nodejs-sdk/dist/lib/Application/Database/WebhookRepository';
 import AConnector from '@orchesty/nodejs-sdk/dist/lib/Connector/AConnector';
 import { HttpMethods } from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
 import ProcessDto from '@orchesty/nodejs-sdk/dist/lib/Utils/ProcessDto';
@@ -12,7 +14,7 @@ export default class BraniUnsubscribeWebhook extends AConnector {
     }
 
     public async processAction(dto: ProcessDto<IInput>): Promise<ProcessDto<IOutput>> {
-        const { id } = dto.getJsonData();
+        const { id, event_type } = dto.getJsonData();
 
         const req = await this.getApplication().getRequestDto(
             dto,
@@ -22,7 +24,18 @@ export default class BraniUnsubscribeWebhook extends AConnector {
         );
         const resp = await this.getSender().send<IOutput>(req, [200]);
 
+        await this.cleanWebhookFromDb(dto.getUser() ?? '', dto.getCurrentApp(), event_type);
+
         return dto.setNewJsonData(resp.getJsonBody());
+    }
+
+    private async cleanWebhookFromDb(user: string, app: string, event: string): Promise<void> {
+        const repo = this.getDbClient().getRepository(Webhook) as WebhookRepository;
+        const registered = await repo.findMany({ users: [user], apps: [app] });
+        const deletedWebhook = registered.find((webhook) => webhook.getName() === event);
+        if (deletedWebhook) {
+            await repo.remove(deletedWebhook);
+        }
     }
 
 }
--- lib/Brani/Connector/__tests__/BraniUnsubscribeWebhook.ts
+++ lib/Brani/Connector/__tests__/BraniUnsubscribeWebhook.ts
@@ -1,4 +1,7 @@
+import { HttpMethods } from '@orchesty/nodejs-sdk/dist/lib/Transport/HttpMethods';
+import { mockOnce } from '@orchesty/nodejs-sdk/dist/test/MockServer';
 import NodeTester from '@orchesty/nodejs-sdk/dist/test/Testers/NodeTester';
+import { devIp } from '../../../../.jest/testEnvs';
 import init from '../../../../test/Implementation/brani';
 import { container } from '../../../../test/TestAbstract';
 import { NAME as BRANI_UNSUBSCRIBE_WEBHOOK } from '../BraniUnsubscribeWebhook';
@@ -12,6 +15,17 @@ describe('Tests for BraniUnsubscribeWebhook', () => {
     });
 
     it('process - ok', async () => {
+        mockOnce([
+            {
+                request: {
+                    method: HttpMethods.GET, url: new RegExp(`http:\\/\\/${devIp}\\/document\\/Webhook.*`),
+                },
+                response: {
+                    code: 200,
+                    body: [{ name: 'balic_packed' }],
+                },
+            }]);
+
         await tester.testConnector(BRANI_UNSUBSCRIBE_WEBHOOK);
     });
 });
