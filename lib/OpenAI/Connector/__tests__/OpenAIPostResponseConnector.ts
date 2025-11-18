2e174ea0e61086e1d06f5369fee95ab7c743c021
Valerij Lunhul <lunhul.v@orchesty-solutions.com>
2025-11-18 13:30:57 +0000

    [OPL-24] Add OpenAI connector and app

---
+++ lib/OpenAI/Connector/__tests__/OpenAIPostResponseConnector.ts
@@ -0,0 +1,17 @@
+import NodeTester from '@orchesty/nodejs-sdk/dist/test/Testers/NodeTester';
+import init from '../../../../test/Implementation/openAi';
+import { container } from '../../../../test/TestAbstract';
+import { NAME as OPEN_AI_POST_RESPONSE_CONNECTOR } from '../OpenAIPostResponseConnector';
+
+let tester: NodeTester;
+
+describe('Tests for OpenAIPostResponseConnector', () => {
+    beforeEach(() => {
+        tester = new NodeTester(container, __filename);
+        init();
+    });
+
+    it('process - ok', async () => {
+        await tester.testConnector(OPEN_AI_POST_RESPONSE_CONNECTOR);
+    });
+});
