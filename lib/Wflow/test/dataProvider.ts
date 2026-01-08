6f12e37b2dd5a1ddee09700ee682e37748566e6b
Valerij Lunhul <lunhul.v@orchesty-solutions.com>
2026-01-08 14:18:23 +0100

    eslint fix

--- lib/Wflow/test/dataProvider.ts
+++ lib/Wflow/test/dataProvider.ts
@@ -1,11 +1,11 @@
-import { appInstall, DEFAULT_USER, DEFAULT_CLIENT_ID, DEFAULT_CLIENT_SECRET, DEFAULT_ACCESS_TOKEN } from '@orchesty/nodejs-connectors/test/DataProvider';
-import { container, db, sender, oauth2Provider } from '@orchesty/nodejs-connectors/test/TestAbstract';
+import { appInstall, DEFAULT_ACCESS_TOKEN, DEFAULT_CLIENT_ID, DEFAULT_CLIENT_SECRET, DEFAULT_USER } from '@orchesty/nodejs-connectors/test/DataProvider';
+import { container, db, oauth2Provider, sender } from '@orchesty/nodejs-connectors/test/TestAbstract';
 import CoreFormsEnum from '@orchesty/nodejs-sdk/dist/lib/Application/Base/CoreFormsEnum';
-import WflowGetDocumentConnector from '../src/Connector/WflowGetDocumentConnector';
-import WflowApplication, { NAME as WFLOW_APP } from '../src/WflowApplication';
-import { CLIENT_ID, CLIENT_SECRET } from '@orchesty/nodejs-sdk/dist/lib/Authorization/Type/OAuth2/IOAuth2Application';
 import { ACCESS_TOKEN } from '@orchesty/nodejs-sdk/dist/lib/Authorization/Provider/OAuth2/OAuth2Provider';
 import { TOKEN } from '@orchesty/nodejs-sdk/dist/lib/Authorization/Type/Basic/ABasicApplication';
+import { CLIENT_ID, CLIENT_SECRET } from '@orchesty/nodejs-sdk/dist/lib/Authorization/Type/OAuth2/IOAuth2Application';
+import WflowGetDocumentConnector from '../src/Connector/WflowGetDocumentConnector';
+import WflowApplication, { NAME as WFLOW_APP } from '../src/WflowApplication';
 
 export default function init(): void {
     appInstall(WFLOW_APP, DEFAULT_USER, {
